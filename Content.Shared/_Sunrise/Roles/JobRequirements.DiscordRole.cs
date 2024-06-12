91df4e5b0329543ade5fb7a7e1d776de7ac6f5aa
Vigers Ray <60344369+VigersRay@users.noreply.github.com>
2024-06-12 16:01:54 +0300

    Отключил ПЦК, ОЦК через роль в дискорде. (#66)

---
+++ Content.Shared/_Sunrise/Roles/JobRequirements.DiscordRole.cs
@@ -0,0 +1,12 @@
+﻿using Content.Shared.Roles;
+using JetBrains.Annotations;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._Sunrise.Roles
+{
+    [UsedImplicitly]
+    [Serializable, NetSerializable]
+    public sealed partial class DiscordRoleRequirement : JobRequirement
+    {
+    }
+}
