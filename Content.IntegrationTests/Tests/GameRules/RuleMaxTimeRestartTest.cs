7058a27c633041f7b05e2e3ed603c193ea083f22
Rinary <rinary.super@gmail.com>
2024-06-14 10:18:10 +0300

    Restart Test Fix

--- Content.IntegrationTests/Tests/GameRules/RuleMaxTimeRestartTest.cs
+++ Content.IntegrationTests/Tests/GameRules/RuleMaxTimeRestartTest.cs
@@ -54,6 +54,18 @@ namespace Content.IntegrationTests.Tests.GameRules
             await server.WaitAssertion(() =>
             {
                 Assert.That(sGameTicker.RunLevel, Is.EqualTo(GameRunLevel.PostRound));
+				
+				var gameRuleEntities = entityManager.GetEntities<GameRuleComponent>().ToList();
+                foreach (var entity in gameRuleEntities)
+                {
+                    entityManager.DeleteEntity(entity);
+                }
+
+                var activeGameRuleEntities = entityManager.GetEntities<ActiveGameRuleComponent>().ToList();
+                foreach (var entity in activeGameRuleEntities)
+                {
+                    entityManager.DeleteEntity(entity);
+                }
             });
 
             ticks = sGameTiming.TickRate * (int) Math.Ceiling(maxTime.RoundEndDelay.TotalSeconds * 1.1f);
