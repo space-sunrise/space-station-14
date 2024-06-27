9ba8c6c6157ce9f9b0caab35263ec0cebf29a7e5
VigersRay <VigersRay@gmail.com>
2024-06-27 12:57:27 +0300

    Revert "Следы от луж (#152)"
    This reverts commit 1355f71e4b7c945a0265f4c796ad132f8d355754.

--- Content.Server/_Sunrise/FootPrints/Components/PuddleFootPrintsComponent.cs
+++
@@ -1,8 +0,0 @@
-namespace Content.Server.FootPrints.Components;
-
-[RegisterComponent]
-public sealed partial class PuddleFootPrintsComponent : Component
-{
-    public float SizeRatio = 0.3f;
-    public float OffPercent = 80f;
-}
