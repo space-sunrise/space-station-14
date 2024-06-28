092f19705df646a15e19a0958c98d457164281a6
VigersRay <VigersRay@gmail.com>
2024-06-28 10:49:13 +0300

    Ну типа да

--- Content.Client/_Sunrise/TTS/TTSSystem.cs
+++ Content.Client/_Sunrise/TTS/TTSSystem.cs
@@ -103,7 +103,8 @@ public sealed class TTSSystem : EntitySystem
         if (_announcementUid == EntityUid.Invalid)
             _announcementUid = Spawn(null);
 
-        var finalParams = AudioParams.Default.WithVolume(SharedAudioSystem.GainToVolume(_volumeAnnounce));
+        var volume = SharedAudioSystem.GainToVolume(_volumeAnnounce);
+        var finalParams = AudioParams.Default.WithVolume(volume);
 
         if (ev.AnnouncementSound != null)
         {
