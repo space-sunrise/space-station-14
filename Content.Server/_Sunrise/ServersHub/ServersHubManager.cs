b604d9996626c4a3d6c5576c8dada55a7aafe63b
VigersRay <VigersRay@gmail.com>
2024-07-01 20:50:16 +0300

    с

--- Content.Server/_Sunrise/ServersHub/ServersHubManager.cs
+++ Content.Server/_Sunrise/ServersHub/ServersHubManager.cs
@@ -84,10 +84,26 @@ public sealed partial class ServersHubManager
         }
     }
 
+    private void Update(FrameEventArgs frameEventArgs)
+    {
+        ServerUpTime.Set(_uptimeStopwatch.Elapsed.TotalSeconds);
+
+        _modLoader.BroadcastUpdate(ModUpdateLevel.FramePreEngine, frameEventArgs);
+
+        _watchdogApi.Heartbeat();
+        _hubManager.Heartbeat();
+
+        _modLoader.BroadcastUpdate(ModUpdateLevel.FramePostEngine, frameEventArgs);
+
+        _metricsManager.FrameUpdate();
+    }
+
     private async Task UpdateServerData()
     {
         if (_serversList.Count == 0)
             return;
+
+        _cfg.SetCVar(CVars.ResourceUploadingLimitMb, 0f);
 
         _serverDataList.Clear();
 
