cae70552d4e3a3b4d90e54c9464385f5a6612adc
Rinary <rinary.super@gmail.com>
2024-07-02 13:50:20 +0300

    Убрал изменение прелоада, добавил таймер у голосования в 120 секунд

--- Content.Server/GameTicking/GameTicker.Lobby.cs
+++ Content.Server/GameTicking/GameTicker.Lobby.cs
@@ -19,7 +19,7 @@ namespace Content.Server.GameTicking
         /// How long before RoundStartTime do we load maps.
         /// </summary>
         [ViewVariables]
-        public TimeSpan RoundPreloadTime { get; } = TimeSpan.FromSeconds(120);
+        public TimeSpan RoundPreloadTime { get; } = TimeSpan.FromSeconds(15);
 
         [ViewVariables]
         private TimeSpan _pauseTime;
--- Content.Server/_Sunrise/RoundEndVote/RoundEndVoteSystem.cs
+++ Content.Server/_Sunrise/RoundEndVote/RoundEndVoteSystem.cs
@@ -2,6 +2,7 @@ using Content.Server.GameTicking;
 using Content.Server.RoundEnd;
 using Content.Server.Voting.Managers;
 using Content.Shared.Voting;
+using System.Threading.Tasks;
 
 namespace Content.Server._Sunrise.RoundEndVote;
 
@@ -9,6 +10,8 @@ public sealed class RoundEndVoteSystem : EntitySystem
 {
     [Dependency] private readonly GameTicker _gameTicker = default!;
     [Dependency] private readonly IVoteManager _voteManager = default!;
+	
+	private bool _isWaitingForVote = false;
 
     public override void Initialize()
     {
@@ -17,14 +20,23 @@ public sealed class RoundEndVoteSystem : EntitySystem
         SubscribeLocalEvent<RoundEndSystemChangedEvent>(OnRoundEndSystemChange);
     }
 
-    private void OnRoundEndSystemChange(RoundEndSystemChangedEvent args)
+    private async void OnRoundEndSystemChange(RoundEndSystemChangedEvent args)
     {
         if (_gameTicker.RunLevel != GameRunLevel.PreRoundLobby)
             return;
+		
+		if (_isWaitingForVote)
+            return;
+
+        _isWaitingForVote = true;
+		
+		await Task.Delay(TimeSpan.FromSeconds(120));
 
         // Очень жаль
         //_voteManager.CreateStandardVote(null, StandardVoteType.Preset);
         _voteManager.CreateStandardVote(null, StandardVoteType.Map);
         _gameTicker.SetGamePreset("Secret");
+		
+		_isWaitingForVote = false;
     }
 }
