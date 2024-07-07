5e29678befa63eae079bef606dddd573d5ffc58f
pxc1984 <igor.mamaev1@gmail.com>
2024-07-07 17:16:47 +0300

    Добавил автокрио
    Signed-off-by: JDtrimble <igor.mamaev1@gmail.com>

---
+++ Content.Server/_Sunrise/CryoTeleport/CryoTeleportTargetComponent.cs
@@ -0,0 +1,14 @@
+﻿namespace Content.Server._Sunrise.CryoTeleport;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed partial class CryoTeleportTargetComponent : Component
+{
+    [DataField]
+    public EntityUid? Station;
+
+    [DataField]
+    public TimeSpan? ExitTime;
+}
