0106e689a397cda8723cb6df8d7de93082d99561
pacable <77161122+pxc1984@users.noreply.github.com>
2024-07-12 00:11:18 +0300

    датасет и система
    Signed-off-by: JDtrimble <igor.mamaev1@gmail.com>

---
+++ Content.Server/_Sunrise/NameGenerators/ShuttleNameGeneratorComponent.cs
@@ -0,0 +1,14 @@
+﻿using Content.Shared.Dataset;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server._Sunrise.NameGenerators;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed partial class ShuttleNameGeneratorComponent : Component
+{
+    [DataField]
+    public ProtoId<DatasetPrototype> NameFragments = "ru_shuttle_prefixes";
+}
---
+++ Content.Server/_Sunrise/NameGenerators/ShuttleNameGeneratorSystem.cs
@@ -0,0 +1,29 @@
+﻿using Robust.Shared.Prototypes;
+using Robust.Shared.Random;
+
+namespace Content.Server._Sunrise.NameGenerators;
+
+/// <summary>
+/// This handles...
+/// </summary>
+public sealed class ShuttleNameGeneratorSystem : EntitySystem
+{
+    [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+    [Dependency] private readonly MetaDataSystem _meta = default!;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<ShuttleNameGeneratorComponent, ComponentInit>(OnComponentInit);
+    }
+
+    private void OnComponentInit(EntityUid uid, ShuttleNameGeneratorComponent component, ComponentInit args)
+    {
+        if (!TryComp<MetaDataComponent>(uid, out var metaDataComponent))
+            return;
+
+        var prefix = _random.Pick(_prototypeManager.Index(component.NameFragments).Values);
+        _meta.SetEntityName(uid, $"{prefix}-{metaDataComponent.EntityName} {Math.Ceiling(_random.NextFloat(1f, 10f) * 100)}", metaDataComponent, false);
+    }
+}
