15c9545d31eeaf497ee5ca74f4fb33bfbdcb244f
Babaev <129369024+babaevlsdd@users.noreply.github.com>
2024-07-12 01:23:02 +0000

    Update SuitSensorComponent.cs (#183)

--- Content.Server/Medical/SuitSensors/SuitSensorComponent.cs
+++ Content.Server/Medical/SuitSensors/SuitSensorComponent.cs
@@ -15,7 +15,7 @@ public sealed partial class SuitSensorComponent : Component
     ///     Choose a random sensor mode when item is spawned.
     /// </summary>
     [DataField("randomMode")]
-    public bool RandomMode = true;
+    public bool RandomMode = false; // Sunrise-edit
 
     /// <summary>
     ///     If true user can't change suit sensor mode
@@ -27,7 +27,7 @@ public sealed partial class SuitSensorComponent : Component
     ///     Current sensor mode. Can be switched by user verbs.
     /// </summary>
     [DataField("mode")]
-    public SuitSensorMode Mode = SuitSensorMode.SensorOff;
+    public SuitSensorMode Mode = SuitSensorMode.SensorCords; // Sunrise-edit
 
     /// <summary>
     ///     Activate sensor if user wear it in this slot.
