0277e6e46042675024d7d1c11b8fc4d5bb24a571
pacable <77161122+pxc1984@users.noreply.github.com>
2024-07-12 04:30:44 +0300

    Реворк оружейки гамма кода (#177)
    * Реворк оружейки гамма кода
    
    * Добавил объявление
    
    * фикс гравы
    
    * оформление
    
    * фикс линтера
    
    * фикс дельты и гравы
    
    Signed-off-by: JDtrimble <igor.mamaev1@gmail.com>
    
    ---------
    
    Signed-off-by: JDtrimble <igor.mamaev1@gmail.com>
    Co-authored-by: Vigers Ray <60344369+VigersRay@users.noreply.github.com>

---
+++ Content.Server/_Sunrise/Shuttles/CodeEquipmentComponent.cs
@@ -0,0 +1,18 @@
+﻿using Robust.Shared.Utility;
+
+namespace Content.Server._Sunrise.Shuttles;
+
+[RegisterComponent]
+public sealed partial class CodeEquipmentComponent : Component
+{
+    public List<EntityUid> Shuttles = [];
+
+    [DataField("shuttlePath")]
+    public ResPath ShuttlePath = new("Maps/_Sunrise/Shuttles/gamma_armory.yml");
+
+    [DataField]
+    public string TargetCode = "gamma";
+
+    [DataField]
+    public string PriorityTag = "DockGamma";
+}
