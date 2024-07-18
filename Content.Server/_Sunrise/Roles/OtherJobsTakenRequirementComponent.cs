e5658dcdbabe4f49d19ce4df20967b531ec77a66
pacable <77161122+pxc1984@users.noreply.github.com>
2024-07-18 17:28:46 +0300

    Фикс КТ + несколько мелких изменений (#214)
    * у ОСЩ майндщилд
    
    Signed-off-by: JDtrimble <igor.mamaev1@gmail.com>
    
    * Revert "у ОСЩ майндщилд"
    
    This reverts commit 595f8867cc29c5cf248d9da06c03de22ed80eae4.
    
    * Исправлено выделение сообщений в чате некоторых ролей. Исправлено расположение ролей в меню выбора персонажа
    
    Signed-off-by: JDtrimble <igor.mamaev1@gmail.com>
    
    * Убраны играбельные ксеносы из космической тюрьмы. Исправлен спавн шаттла пилота КТ
    
    Signed-off-by: JDtrimble <igor.mamaev1@gmail.com>
    
    * Теперь шаттл летит мгновенно
    
    Signed-off-by: JDtrimble <igor.mamaev1@gmail.com>
    
    * Update blueshield.yml
    
    ---------
    
    Signed-off-by: JDtrimble <igor.mamaev1@gmail.com>
    Co-authored-by: Vigers Ray <60344369+VigersRay@users.noreply.github.com>

---
+++ Content.Server/_Sunrise/Roles/OtherJobsTakenRequirementComponent.cs
@@ -0,0 +1,24 @@
+﻿using Content.Shared.Roles;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+
+namespace Content.Server._Sunrise.Roles;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed partial class OtherJobsTakenRequirementComponent : Component
+{
+    [DataField("targetJob", customTypeSerializer: typeof(PrototypeIdSerializer<JobPrototype>))]
+    public string TargetJob;
+
+    [DataField("adjustJob", customTypeSerializer: typeof(PrototypeIdSerializer<JobPrototype>))]
+    public string AdjustJob;
+
+    [DataField("coefficient")]
+    public int Coefficient;
+
+    public int Accumulator = 0;
+}
