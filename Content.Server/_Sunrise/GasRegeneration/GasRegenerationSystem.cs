41eb5866c324c6ce46d0685732d574a2755f4fb2
VigersRay <VigersRay@gmail.com>
2024-07-27 07:50:12 +0300

    Ну типа всё

---
+++ Content.Server/_Sunrise/GasRegeneration/GasRegenerationSystem.cs
@@ -0,0 +1,38 @@
+using Content.Server.Atmos.Components;
+using Content.Server.Atmos.EntitySystems;
+using Robust.Shared.Timing;
+
+namespace Content.Server.Sunrise.GasRegeneration;
+
+public sealed class GasRegenerationSystem : EntitySystem
+{
+    [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private readonly AtmosphereSystem _atmosphereSystem = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<GasRegenerationComponent, EntityUnpausedEvent>(OnUnpaused);
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var query = EntityQueryEnumerator<GasRegenerationComponent, GasTankComponent>();
+        while (query.MoveNext(out var uid, out var gasRegen, out var gasTank))
+        {
+            if (_timing.CurTime < gasRegen.NextRegenTime)
+                continue;
+
+            gasRegen.NextRegenTime = _timing.CurTime + gasRegen.Duration;
+            _atmosphereSystem.Merge(gasTank.Air, gasRegen.AirRegen.Clone());
+        }
+    }
+
+    private void OnUnpaused(EntityUid uid, GasRegenerationComponent comp, ref EntityUnpausedEvent args)
+    {
+        comp.NextRegenTime += args.PausedTime;
+    }
+}
