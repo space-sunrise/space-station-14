3e1961136f564f31b505f2587773239e93d9240e
JDtrimble <igor.mamaev1@gmail.com>
2024-07-29 11:30:26 +0300

    -костыль
    Signed-off-by: JDtrimble <igor.mamaev1@gmail.com>

--- Content.Server/_Sunrise/Smile/SmileSlimeSystem.cs
+++ Content.Server/_Sunrise/Smile/SmileSlimeSystem.cs
@@ -12,6 +12,7 @@ using Robust.Shared.Random;
 using Robust.Shared.Timing;
 using Content.Shared._Sunrise.Smile;
 using Content.Shared.Actions;
+using Content.Shared.Body.Components;
 using Content.Shared.DoAfter;
 using Content.Shared.Movement.Systems;
 
@@ -111,7 +112,7 @@ public sealed class SmileSlimeSystem : EntitySystem
             {
                 if (HasComp<SmileSlimeComponent>(entity))
                     continue;
-                if (!TryComp<HumanoidAppearanceComponent>(entity, out var humanoidAppearanceComponent))
+                if (!TryComp<BodyComponent>(entity, out _))
                     continue;
                 _popupSystem.PopupEntity(Loc.GetString(_random.Pick(smileSlimeComponent.Messages)), entity, entity);
             }
