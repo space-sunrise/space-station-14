44e02e6c0d10816bc363ef3306b0bf8afecd4bcf
JDtrimble <igor.mamaev1@gmail.com>
2024-07-29 11:28:05 +0300

    у смайла есть суперспособность
    Signed-off-by: JDtrimble <igor.mamaev1@gmail.com>

--- Content.Server/_Sunrise/Smile/SmileSlimeSystem.cs
+++ Content.Server/_Sunrise/Smile/SmileSlimeSystem.cs
@@ -1,4 +1,5 @@
-﻿using Content.Shared.Damage;
+﻿using Content.Shared._Sunrise.Smile;
+using Content.Shared.Damage;
 using Content.Shared.Humanoid;
 using Content.Shared.Interaction;
 using Content.Shared.Mobs.Systems;
@@ -9,12 +10,13 @@ using Robust.Shared.Network;
 using Robust.Shared.Physics.Events;
 using Robust.Shared.Random;
 using Robust.Shared.Timing;
+using Content.Shared._Sunrise.Smile;
+using Content.Shared.Actions;
+using Content.Shared.DoAfter;
+using Content.Shared.Movement.Systems;
 
 namespace Content.Server._Sunrise.Smile;
 
-/// <summary>
-/// This handles...
-/// </summary>
 public sealed class SmileSlimeSystem : EntitySystem
 {
     [Dependency] private readonly IGameTiming _gameTiming = default!;
@@ -26,20 +28,73 @@ public sealed class SmileSlimeSystem : EntitySystem
     [Dependency] private readonly INetManager _netMan = default!;
     [Dependency] private readonly EntityManager _entMan = default!;
     [Dependency] private readonly EntityLookupSystem _entityLookup = default!;
+    [Dependency] private readonly SharedActionsSystem _actions = default!;
+    [Dependency] private readonly SharedDoAfterSystem _doAfterSystem = default!;
+    [Dependency] private readonly DamageableSystem _damageableSystem = default!;
 
     private TimeSpan _nextTick = TimeSpan.Zero;
     private readonly TimeSpan _refreshCooldown = TimeSpan.FromSeconds(5);
 
     public override void Initialize()
     {
-        SubscribeLocalEvent<DamageableComponent, GetVerbsEvent<InteractionVerb>>(OnGetVerbs);
+        SubscribeLocalEvent<SmileSlimeComponent, MapInitEvent>(OnMapInit);
+        SubscribeLocalEvent<SmileSlimeComponent, SmileLoveActionEvent>(OnLoveAction);
+        SubscribeLocalEvent<SmileSlimeComponent, ComponentShutdown>(OnCompShutdown);
+        SubscribeLocalEvent<SmileSlimeComponent, SmileLoveDoAfterEvent>(OnDoAfter);
     }
 
-    private void OnGetVerbs(EntityUid target, DamageableComponent comp, GetVerbsEvent<InteractionVerb> args)
+    private void OnMapInit(EntityUid uid, SmileSlimeComponent comp, MapInitEvent args)
     {
-        if (!HasComp<SmileSlimeComponent>(args.User))
+        _actions.AddAction(uid, ref comp.ActionEntity, comp.Action);
+    }
+
+    private void OnCompShutdown(EntityUid uid, SmileSlimeComponent comp, ComponentShutdown args)
+    {
+        _actions.RemoveAction(uid, comp.ActionEntity);
+    }
+
+    private void OnLoveAction(EntityUid smileUid, SmileSlimeComponent comp, SmileLoveActionEvent args)
+    {
+        var doAfter = new DoAfterArgs(EntityManager,
+            smileUid,
+            comp.ActionTime,
+            new SmileLoveDoAfterEvent(),
+            smileUid,
+            args.Target)
+        {
+            BreakOnMove = true,
+            BlockDuplicate = true,
+            BreakOnDamage = true,
+            CancelDuplicate = true,
+        };
+
+        var targetXform = Transform(args.Target);
+        _audio.PlayPvs(comp.SoundSpecifier, targetXform.Coordinates);
+        _entMan.SpawnEntity("EffectHearts", targetXform.Coordinates);
+        _popupSystem.PopupEntity(
+            Loc.GetString(comp.AffectionPopupText,
+                ("slime", MetaData(smileUid).EntityName),
+                ("target", MetaData(args.Target).EntityName)),
+            args.Target,
+            PopupType.Medium);
+        _doAfterSystem.TryStartDoAfter(doAfter);
+
+        args.Handled = true;
+    }
+
+    private void OnDoAfter(EntityUid uid, SmileSlimeComponent comp, SmileLoveDoAfterEvent args)
+    {
+        if (args.Cancelled || args.Handled)
             return;
-        Log.Debug($"OnGetVerbs {args.User} {args.Target}");
+
+        if (args.Target == null)
+            return;
+
+        var targetXform = Transform(args.Target.Value);
+        _entMan.SpawnEntity("EffectHearts", targetXform.Coordinates);
+        _audio.PlayPvs(comp.SoundSpecifier, targetXform.Coordinates);
+        _damageableSystem.TryChangeDamage(args.Target, comp.DamageSpecifier, true, false);
+        args.Handled = true;
     }
 
     public override void Update(float delay)
