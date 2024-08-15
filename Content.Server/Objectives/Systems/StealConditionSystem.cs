9e65e6327a0974e3eba9fbe02bb5509086dba897
Rinary <rinary.super@gmail.com>
2024-08-15 23:53:42 +0300

    ты серьезно? Доебалось до 2х скоб

--- Content.Server/Objectives/Systems/StealConditionSystem.cs
+++ Content.Server/Objectives/Systems/StealConditionSystem.cs
@@ -79,7 +79,7 @@ public sealed class StealConditionSystem : EntitySystem
         var locale = $"objective-{Regex.Replace(group.Name, @"[*?!'%\s]", string.Empty).ToLower()}";
         if (Loc.GetString(locale) == locale)
         {
-            Logger.Error($"Steal item {objective-{Regex.Replace(group.Name, @"[*?!'%\s]", string.Empty).ToLower()}} doesn't have locale");
+            Logger.Error($"Steal item objective-{Regex.Replace(group.Name, @"[*?!'%\s]", string.Empty).ToLower()} doesn't have locale");
             locale = group.Name;
         }
         var title =condition.Comp.OwnerText == null
