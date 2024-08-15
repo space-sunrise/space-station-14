fd11019b281b07f4eae920e0d7d7dceee90c5cc4
VigersRay <vigersray@gmail.com>
2024-08-15 06:49:35 +0300

    фикс GhostThemeComponent

--- Content.Shared/_Sunrise/GhostTheme/GhostThemeComponent.cs
+++ Content.Shared/_Sunrise/GhostTheme/GhostThemeComponent.cs
@@ -6,6 +6,7 @@ namespace Content.Shared._Sunrise.GhostTheme;
 
 public sealed partial class GhostThemeComponent: Component
 {
+    [ViewVariables(VVAccess.ReadOnly)]
     [DataField, AutoNetworkedField]
     public string? GhostTheme;
 }
