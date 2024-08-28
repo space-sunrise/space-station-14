2cf61843edf641280306a8bdbc0e5a6fb444d4b8
VigersRay <vigersray@gmail.com>
2024-08-28 21:31:46 +0300

    Фикс дебаг сборки

--- Content.Client/_Sunrise/ServersHub/ServersHubManager.cs
+++ Content.Client/_Sunrise/ServersHub/ServersHubManager.cs
@@ -1,36 +1,44 @@
-using System.Linq;
 using Content.Shared._Sunrise.ServersHub;
+using Robust.Client.UserInterface;
 using Robust.Shared.Network;
-using Robust.Shared.Timing;
 
 namespace Content.Client._Sunrise.ServersHub;
 
 public partial class ServersHubManager
 {
     [Dependency] private readonly IClientNetManager _netManager = default!;
+    [Dependency] protected readonly IUserInterfaceManager UIManager = default!;
 
     public event Action<List<ServerHubEntry>>? ServersDataListChanged;
 
     public List<ServerHubEntry> ServersDataList = [];
 
-    private ServersHubUi? _menu;
+    private ServersHubUi? _window;
 
     public void Initialize()
     {
         _netManager.RegisterNetMessage<MsgFullServerHubList>(OnServersDataChanged);
-
-        _menu = new ServersHubUi();
     }
 
-    // Ахуеть какой костыль, но явно лучше чем писать лишние 300 строк.
-    public void OpenServersHub()
+    public void ToggleWindow()
     {
-        _menu = new ServersHubUi();
-        _menu.OnClose += _menu.Close;
-        _menu.OpenCentered();
-        var totalPlayers = ServersDataList.Sum(server => server.CurrentPlayers);
-        var maxPlayers = ServersDataList.Sum(server => server.MaxPlayers);
-        _menu!.RefreshHeader(totalPlayers, maxPlayers);
+        EnsureWindow();
+
+        if (_window!.IsOpen)
+        {
+            _window.Close();
+        }
+        else
+        {
+            _window.OpenCentered();
+        }
+    }
+
+    private void EnsureWindow()
+    {
+        if (_window is { Disposed: false })
+            return;
+        _window = UIManager.CreateWindow<ServersHubUi>();
     }
 
     private void OnServersDataChanged(MsgFullServerHubList msg)
@@ -39,8 +47,6 @@ public partial class ServersHubManager
         // и его можно было отобразить после отключения или бана.
         ServersDataList = msg.ServersHubEntries;
         ServersDataListChanged?.Invoke(ServersDataList);
-        var totalPlayers = ServersDataList.Sum(server => server.CurrentPlayers);
-        var maxPlayers = ServersDataList.Sum(server => server.MaxPlayers);
-        _menu!.RefreshHeader(totalPlayers, maxPlayers);
+        _window!.RefreshHeader();
     }
 }
