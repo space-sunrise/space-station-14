633bb896b7a1cd075c8f4865ceba7ee69456e695
pacable <igor.mamaev1@gmail.com>
2024-10-05 22:07:23 +0300

    fast
    Signed-off-by: pacable <igor.mamaev1@gmail.com>

---
+++ Content.Shared/Changeling/ChangelingHiveComponent.cs
@@ -0,0 +1,11 @@
+﻿namespace Content.Shared.Changeling;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed partial class ChangelingHiveComponent : Component
+{
+    [DataField]
+    public ChangelingHive? Hive = null;
+}
