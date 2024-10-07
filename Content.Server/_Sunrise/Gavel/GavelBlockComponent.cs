3d4d6f466e2311f6a3818d90d57a2d7255c7d5a5
pacable <igor.mamaev1@gmail.com>
2024-10-07 22:22:28 +0300

    судейский молоточек и наковальня
    Signed-off-by: pacable <igor.mamaev1@gmail.com>

---
+++ Content.Server/_Sunrise/Gavel/GavelBlockComponent.cs
@@ -0,0 +1,15 @@
+﻿using Robust.Shared.Audio;
+
+namespace Content.Server._Sunrise.Gavel;
+
+[RegisterComponent]
+public sealed partial class GavelBlockComponent : Component
+{
+    [DataField]
+    public SoundSpecifier HitSound;
+
+    [DataField]
+    public TimeSpan Cooldown = TimeSpan.FromSeconds(1.5);
+
+    public TimeSpan? PrevSound;
+}
---
+++ Content.Server/_Sunrise/Gavel/GavelSystem.cs
@@ -0,0 +1,28 @@
+﻿using Content.Shared.Interaction;
+using Robust.Shared.Audio.Systems;
+using Robust.Shared.Timing;
+
+namespace Content.Server._Sunrise.Gavel;
+
+public sealed class GavelSystem : EntitySystem
+{
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
+
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<GavelHammerComponent, AfterInteractEvent>(OnHit);
+    }
+
+    private void OnHit(EntityUid uid, GavelHammerComponent component, AfterInteractEvent args)
+    {
+        if (args.Target == null || !TryComp<GavelBlockComponent>(args.Target, out var comp))
+            return;
+
+        if ((comp.PrevSound != null && _timing.CurTime - comp.PrevSound > comp.Cooldown) || comp.PrevSound == null)
+        {
+            _audio.PlayPvs(comp.HitSound, args.Target.Value);
+            comp.PrevSound = _timing.CurTime;
+        }
+    }
+}
