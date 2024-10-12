e006ad27d4f29c45446d60fb2a725b7459566e6b
Rinary <rinary.super@gmail.com>
2024-10-12 14:10:03 +0300

    upd

--- Tools/_sunrise/Schemas/validate_yml.py
+++ Tools/_sunrise/Schemas/validate_yml.py
@@ -4,7 +4,7 @@ import argparse
 import os
 import re
 from glob import iglob
-from typing import Any, List
+from typing import List
 
 errors: List["LocaleError"] = []
 
@@ -18,7 +18,7 @@ def main() -> int:
         check_dir(dir)
 
     for error in errors:
-        print(f"{error.path}: {error.message}")
+        print(f"{error.path} (строка {error.line}): {error.message}")
 
     return 1 if errors else 0
 
@@ -30,41 +30,34 @@ def check_dir(dir: str):
 def check_yml(yml_path: str):
     try:
         with open(yml_path, "r", encoding="utf-8") as file:
-            content = file.read()
+            content = file.readlines()
 
-            # Оставляем только строки с ключами 'name:', 'description:', 'suffix:'
-            filtered_content = filter_specific_keys(content)
-
-            # Проверка нужных полей на русские символы
-            for key, value in filtered_content.items():
-                if has_russian_chars(value):
-                    add_error(yml_path, f"Поле '{key}' содержит русские символы.")
+            # Оставляем только строки с ключами 'name:', 'description:', 'suffix:', 'rules:'
+            filter_and_check(content, yml_path)
 
     except Exception as e:
-        add_error(yml_path, f"Ошибка чтения файла: {e}")
+        add_error(yml_path, -1, f"Ошибка чтения файла: {e}")
 
-def filter_specific_keys(content: str) -> dict:
-    result = {}
-    lines = content.splitlines()
+def filter_and_check(content: List[str], yml_path: str):
     key_pattern = re.compile(r'^(name|description|suffix|rules):\s*(.+)')
 
-    for line in lines:
+    for i, line in enumerate(content, start=1):
         match = key_pattern.match(line.strip())
         if match:
             key, value = match.groups()
-            result[key] = value
-
-    return result
+            if has_russian_chars(value):
+                add_error(yml_path, i, f"Поле '{key}' содержит русские символы.")
 
 def has_russian_chars(text: str) -> bool:
     return bool(re.search(r'[а-яА-Я]', text))
 
-def add_error(yml: str, message: str):
-    errors.append(LocaleError(yml, message))
+def add_error(yml: str, line: int, message: str):
+    errors.append(LocaleError(yml, line, message))
 
 class LocaleError:
-    def __init__(self, path: str, message: str):
+    def __init__(self, path: str, line: int, message: str):
         self.path = path
+        self.line = line
         self.message = message
 
 exit(main())
