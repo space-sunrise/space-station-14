cc2ffdbe73460ffa6de4320b57de1e58920ec9d4
Rinary <rinary.super@gmail.com>
2024-10-28 18:30:48 +0200

    init

--- Content.Server/_Sunrise/RoundEndVote/RoundEndVoteSystem.cs
+++ Content.Server/_Sunrise/RoundEndVote/RoundEndVoteSystem.cs
@@ -1,8 +1,11 @@
+using System.Threading.Tasks;
 using Content.Server.GameTicking;
 using Content.Server.RoundEnd;
 using Content.Server.Voting.Managers;
 using Content.Shared._Sunrise.SunriseCCVars;
 using Content.Shared.Voting;
+using Content.Shared.GameTicking;
+using Robust.Shared.Timing;
 using Robust.Shared.Configuration;
 
 namespace Content.Server._Sunrise.RoundEndVote;
@@ -10,6 +13,8 @@ namespace Content.Server._Sunrise.RoundEndVote;
 public sealed class RoundEndVoteSystem : EntitySystem
 {
     [Dependency] private readonly GameTicker _gameTicker = default!;
+    [Dependency] private readonly IGameTiming _gameTiming = default!;
+    [Dependency] private readonly SharedGameTicker _sharedgameTicker = default!;
     [Dependency] private readonly IVoteManager _voteManager = default!;
     [Dependency] private readonly IConfigurationManager _cfg = default!;
 
@@ -19,8 +24,20 @@ public sealed class RoundEndVoteSystem : EntitySystem
 
         SubscribeLocalEvent<RoundEndSystemChangedEvent>(OnRoundEndSystemChange);
     }
-
-    private void OnRoundEndSystemChange(RoundEndSystemChangedEvent args)
+    
+    public async void OnRoundEndSystemChange(RoundEndSystemChangedEvent args)
+    {
+        if (_gameTicker.RunLevel != GameRunLevel.PreRoundLobby)
+            return;
+        
+        var delay = Math.Abs((_gameTicker.LobbyDuration - TimeSpan.FromSeconds(75)).TotalMilliseconds);
+        
+        Log.Warning($"waiting {(int)delay}");
+        await Task.Delay((int)delay);
+        StartRoundEndVotes();
+    }
+    
+    public void StartRoundEndVotes()
     {
         if (_gameTicker.RunLevel != GameRunLevel.PreRoundLobby)
             return;
