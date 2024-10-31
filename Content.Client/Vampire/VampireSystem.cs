9bc95bc482deac0e83238d9c01a0ec714120d096
Rinary <rinary.super@gmail.com>
2024-11-01 01:13:14 +0200

    upd

--- Content.Client/Vampire/VampireSystem.cs
+++ Content.Client/Vampire/VampireSystem.cs
@@ -18,8 +18,8 @@ public sealed partial class VampireSystem : EntitySystem
     }
     
     private void GetVampireIcon(Entity<VampireComponent> ent, ref GetStatusIconsEvent args)
-    {
-        if (_prototype.TryIndex(ent.Comp.StatusIcon, out var iconPrototype))
-            args.StatusIcons.Add(iconPrototype);
+    {   
+        var iconPrototype = _prototype.Index(ent.Comp.StatusIcon);
+        args.StatusIcons.Add(iconPrototype);
     }
 }
