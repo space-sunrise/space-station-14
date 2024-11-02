6374cb83cce606f4c6697345a33c0167ce0ff8aa
pacable <igor.mamaev1@gmail.com>
2024-11-02 18:47:03 +0300

    :choo:
    Signed-off-by: pacable <igor.mamaev1@gmail.com>

--- Content.Server/_Sunrise/CentCom/CentComConsoleSystem.cs
+++ Content.Server/_Sunrise/CentCom/CentComConsoleSystem.cs
@@ -4,24 +4,38 @@ using Content.Server.Station.Systems;
 using Content.Shared._Sunrise.CentCom;
 using Content.Shared._Sunrise.CentCom.BUIStates;
 using Content.Shared.Construction.Components;
+using Content.Shared.Containers.ItemSlots;
 using Robust.Server.GameObjects;
 using Robust.Shared.Containers;
 
 namespace Content.Server._Sunrise.CentCom;
 
-public sealed class CentComConsoleSystem : SharedCentComConsoleSystem
+public sealed class CentComConsoleSystem : EntitySystem
 {
     [Dependency] private readonly UserInterfaceSystem _userInterface = default!;
     [Dependency] private readonly TransformSystem _transform = default!;
     [Dependency] private readonly StationSystem _station = default!;
+    [Dependency] private readonly ItemSlotsSystem _itemSlotsSystem = default!;
     /// <inheritdoc/>
     public override void Initialize()
     {
+        SubscribeLocalEvent<CentComConsoleComponent, ComponentInit>(OnComponentInit);
+        SubscribeLocalEvent<CentComConsoleComponent, ComponentRemove>(OnComponentRemove);
         SubscribeLocalEvent<CentComConsoleComponent, UnanchorAttemptEvent>(OnUnanchor);
         SubscribeLocalEvent<CentComConsoleComponent, AnchorStateChangedEvent>(OnAnchorStateChanged);
         SubscribeLocalEvent<CentComConsoleComponent, EntInsertedIntoContainerMessage>(UpdateUi);
         SubscribeLocalEvent<CentComConsoleComponent, EntRemovedFromContainerMessage>(UpdateUi);
         SubscribeLocalEvent<CentComConsoleComponent, BoundUIOpenedEvent>(UpdateUi);
+    }
+
+    private void OnComponentInit(EntityUid uid, CentComConsoleComponent component, ComponentInit args)
+    {
+        _itemSlotsSystem.AddItemSlot(uid, CentComConsoleComponent.IdCardSlotId, component.IdSlot);
+    }
+
+    private void OnComponentRemove(EntityUid uid, CentComConsoleComponent component, ComponentRemove args)
+    {
+        _itemSlotsSystem.RemoveItemSlot(uid, component.IdSlot);
     }
 
     private void UpdateStation(EntityUid uid, CentComConsoleComponent component)
