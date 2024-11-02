67e76b4b7e322046c7db8d3662ef75f062ef40a1
Rinary <rinary.super@gmail.com>
2024-11-02 21:37:58 +0200

    fix

--- Content.Server/Administration/Managers/BanManager.cs
+++ Content.Server/Administration/Managers/BanManager.cs
@@ -222,8 +222,8 @@ public sealed partial class BanManager : IBanManager, IPostInjectInit
             ("name", targetName),
             ("ip", addressRangeString),
             ("hwid", hwidString),
-            ("reason", reason)),
-            ("round", roundId);
+            ("reason", reason),
+            ("round", roundId));
 
         _sawmill.Info(logMessage);
         _chat.SendAdminAlert(logMessage);
