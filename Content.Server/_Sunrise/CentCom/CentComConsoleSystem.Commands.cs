68796b20b4ebcbcefa4a406611902c6d7da99e1a
pacable <igor.mamaev1@gmail.com>
2024-11-07 20:23:26 +0300

    spawnas команда
    Signed-off-by: pacable <igor.mamaev1@gmail.com>

--- Content.Server/_Sunrise/CentCom/CentComConsoleSystem.Commands.cs
+++ Content.Server/_Sunrise/CentCom/CentComConsoleSystem.Commands.cs
@@ -1,6 +1,11 @@
-﻿using Content.Server.GameTicking;
+﻿using Content.Server.Administration;
+using Content.Server.GameTicking;
 using Content.Server.Mind;
 using Content.Server.Station.Systems;
+using Content.Shared.Administration;
+using Content.Shared.Clothing;
+using Robust.Server.Console;
+using Robust.Server.Player;
 using Robust.Server.Toolshed.Commands.Players;
 using Robust.Shared.Console;
 using Robust.Shared.Player;
@@ -13,26 +18,36 @@ public sealed partial class CentComConsoleSystem
     [Dependency] private readonly StationSpawningSystem _spawning = default!;
     [Dependency] private readonly MindSystem _mindSystem = default!;
     [Dependency] private readonly GameTicker _ticker = default!;
+    [Dependency] private readonly IServerConsoleHost _serverConsole = default!;
+    [Dependency] private readonly IPlayerLocator _locator = default!;
+    [Dependency] private readonly IPlayerManager _player = default!;
 
     private void InitializeCommands()
     {
-        _consoleHost.RegisterCommand("spawnascentcomoperator", SpawnAsCentcomOperatorCallback);
+        _consoleHost.RegisterCommand("spawnas",
+            "",
+            "spawnas <player> <outfit ID>",
+            SpawnAsCommand);
     }
 
-    private void SpawnAsCentcomOperatorCallback(IConsoleShell shell, string argstr, string[] args)
+    [AdminCommand(AdminFlags.Debug)]
+    private async void SpawnAsCommand(IConsoleShell shell, string argstr, string[] args)
     {
-        if (args.Length != 1)
+        if (args.Length != 2)
             return;
 
-        if (!NetEntity.TryParse(args[0], out var uidNet) || !TryGetEntity(uidNet, out var uid))
-        {
+        if (!_player.TryGetSessionByUsername(args[0], out var session))
             return;
-        }
+
+        if (session.AttachedEntity == null)
+            return;
+
+        var uid = session.AttachedEntity.Value;
 
         if (!TryComp<ActorComponent>(uid, out var targetActor))
             return;
 
-        if (!_transform.TryGetMapOrGridCoordinates(uid.Value, out var coords))
+        if (!_transform.TryGetMapOrGridCoordinates(uid, out var coords))
         {
             return;
         }
@@ -41,12 +56,13 @@ public sealed partial class CentComConsoleSystem
 
         var profile = _ticker.GetPlayerProfile(targetActor.PlayerSession);
         var mobUid = _spawning.SpawnPlayerMob(coords.Value, null, profile, stationUid);
-        var targetMind = _mindSystem.GetMind(uid.Value);
+        var targetMind = _mindSystem.GetMind(uid);
 
         if (targetMind != null)
         {
             _mindSystem.TransferTo(targetMind.Value, mobUid, true);
         }
-        // ты тут закончил
+
+        _serverConsole.ExecuteCommand($"setoutfit {mobUid} {args[1]}");
     }
 }
