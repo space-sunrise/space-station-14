e9f2d09b5fe3f19d33d55cba71fd2c02e6dd3c2e
Vigers Ray <vigersray@gmail.com>
2024-11-13 10:55:02 +0300

    дэм

--- Content.Shared/_Sunrise/SunriseCCVars/SunriseCCVars.cs
+++ Content.Shared/_Sunrise/SunriseCCVars/SunriseCCVars.cs
@@ -281,13 +281,13 @@ public sealed class SunriseCCVars
 
     public static readonly CVarDef<bool> RunPresetVoteAfterRestart = CVarDef.Create("vote.run_preset_vote_after_restart", false);
 
-    public static readonly CVarDef<int> VotingsDelay = CVarDef.Create("vote.votings_delay", 90);
+    public static readonly CVarDef<int> VotingsDelay = CVarDef.Create("vote.votings_delay", 45);
 
     public static readonly CVarDef<int> MapVotingCount = CVarDef.Create("vote.map_voting_count", 3);
 
     public static readonly CVarDef<int> RoundVotingCount = CVarDef.Create("vote.round_voting_count", 3);
 
-    public static readonly CVarDef<string> RoundVotingChancesPrototype = CVarDef.Create("vote.round_voting_chances_prototype", "");
+    public static readonly CVarDef<string> RoundVotingChancesPrototype = CVarDef.Create("vote.round_voting_chances_prototype", "SunriseVoteSecret");
 
     /*
      * Preset
