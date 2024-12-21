f5cf0210cde58d43d9ee1b0e5ef7b7a4b7690184
lunarica <splatt.pr@gmail.com>
2024-12-21 15:44:19 +0500

    work

---
+++ Content.Shared/Standing/StandingStateEvents.cs
@@ -0,0 +1,21 @@
+﻿namespace Content.Shared.Standing;
+
+public sealed class DropHandItemsEvent : EventArgs
+{
+}
+
+public sealed class DownAttemptEvent : CancellableEntityEventArgs
+{
+}
+
+public sealed class StandAttemptEvent : CancellableEntityEventArgs
+{
+}
+
+public sealed class StoodEvent : EntityEventArgs
+{
+}
+
+public sealed class DownedEvent : EntityEventArgs
+{
+}
