66ecbabc928716d45ede2d5ef5f8c73f42404cac
Babaev <129369024+babaevlsdd@users.noreply.github.com>
2025-01-01 16:06:53 +0600

    баланс

--- Content.Shared/_Sunrise/Flip/FlipOnAttackComponent.cs
+++ Content.Shared/_Sunrise/Flip/FlipOnAttackComponent.cs
@@ -3,5 +3,9 @@ using Robust.Shared.GameStates;
 namespace Content.Shared._Sunrise.Flip
 {
     [NetworkedComponent, RegisterComponent]
-    public sealed partial class FlipOnAttackComponent : Component;
+    public sealed partial class FlipOnAttackComponent : Component
+    {
+        [ViewVariables(VVAccess.ReadWrite), DataField("probability")]
+        public float Probability = 1.0f;
+    }
 }
