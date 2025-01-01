3071f353f15f1aa52eccebd990af830e19217713
Vigers Ray <vigersray@gmail.com>
2025-01-01 07:34:11 +0300

    Фиксы удалённого управления шаттлом

--- Content.Shared/_Sunrise/AssaultOps/AssaultOpsShuttleComponent.cs
+++ Content.Shared/_Sunrise/AssaultOps/AssaultOpsShuttleComponent.cs
@@ -1,6 +1,8 @@
+using Robust.Shared.GameStates;
+
 namespace Content.Server._Sunrise.AssaultOps;
 
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent]
 public sealed partial class AssaultOpsShuttleComponent : Component
 {
     [DataField]
