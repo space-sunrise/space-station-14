7acb8c907cef8225c2a89958fc1ff1708362a0df
Vigers Ray <vigersray@gmail.com>
2025-01-11 07:52:02 +0300

    фиксы

--- Content.Client/_Sunrise/UserInterface/Radial/RadialContainer.xaml.cs
+++ Content.Client/_Sunrise/UserInterface/Radial/RadialContainer.xaml.cs
@@ -12,40 +12,9 @@ using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
 using Robust.Client.UserInterface.XAML;
 using Robust.Shared.Animations;
-using Robust.Shared.Console;
-using Robust.Shared.Random;
 using Robust.Shared.Timing;
 
 namespace Content.Client._Sunrise.UserInterface.Radial;
-
-public sealed class RadialContainerCommandTest : LocalizedCommands
-{
-    [Dependency] private readonly IRobustRandom _robustRandom = default!;
-
-    public override string Command => "radialtest";
-
-    public override void Execute(IConsoleShell shell, string argStr, string[] args)
-    {
-        string[] tips =
-        {
-            "Testovый туултип. Здесь можете расписать разную инфу о кнопке/действии",
-            "Из окна дуло. Штирлиц закрыл окно. Дуло исчезло.",
-        };
-        var radial = new RadialContainer();
-        for (int i = 0; i < 8; i++)
-        {
-            var testButton = radial.AddButton("Action " + i, "/Textures/Interface/emotions.svg.192dpi.png");
-            testButton.Tooltip = tips[_robustRandom.Next(0, 2)];
-            testButton.Controller.OnPressed += (_) => { Logger.Debug("Press gay"); };
-        }
-
-        radial.CloseButton.Controller.OnPressed += (_) =>
-        {
-            Logger.Debug("Close event for your own logic");
-        };
-        radial.OpenAttachedLocalPlayer();
-    }
-}
 
 [GenerateTypedNameReferences, Virtual]
 public partial class RadialContainer : Control
@@ -203,7 +172,7 @@ public partial class RadialContainer : Control
         return button;
     }
 
-    public RadialButton AddButton(string action, Texture? texture)
+    public RadialButton AddButton(string action, Robust.Client.Graphics.Texture? texture)
     {
         var button = new RadialButton();
         button.Content = action;
