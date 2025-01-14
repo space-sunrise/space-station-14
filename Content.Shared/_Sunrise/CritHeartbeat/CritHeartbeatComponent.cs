ad879ff67682cbe8a87c6f989307459708b206c8
drdth <drdtheuser@gmail.com>
2025-01-14 16:11:18 +0300

    add: crit heartbeat sound

---
+++ Content.Shared/_Sunrise/CritHeartbeat/CritHeartbeatComponent.cs
@@ -0,0 +1,13 @@
+﻿using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._Sunrise.CritHeartbeat;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class CritHeartbeatComponent : Component
+{
+    [DataField]
+    public SoundSpecifier HeartbeatSound = new SoundPathSpecifier("/Audio/_Sunrise/Effects/heartbeat.ogg");
+
+    public EntityUid? AudioStream;
+}
---
+++ Content.Shared/_Sunrise/CritHeartbeat/CritHeartbeatSystem.cs
@@ -0,0 +1,24 @@
+﻿using Content.Shared.Mobs;
+using Robust.Shared.Audio;
+using Robust.Shared.Audio.Systems;
+
+namespace Content.Shared._Sunrise.CritHeartbeat;
+
+public sealed class CritHeartbeatSystem : EntitySystem
+{
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<CritHeartbeatComponent, MobStateChangedEvent>(OnMobStateChanged);
+    }
+
+    private void OnMobStateChanged(Entity<CritHeartbeatComponent> ent, ref MobStateChangedEvent args)
+    {
+        ent.Comp.AudioStream = args.NewMobState == MobState.Critical
+            ? _audio.PlayEntity(ent.Comp.HeartbeatSound, ent, ent, AudioParams.Default.WithLoop(true))?.Entity
+            : _audio.Stop(ent.Comp.AudioStream);
+    }
+}
