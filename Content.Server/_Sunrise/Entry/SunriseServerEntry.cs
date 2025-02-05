cfbd768f5396302ec5cf52a8d6d8561dacd77921
Vigers Ray <vigersray@gmail.com>
2025-02-05 03:40:19 +0300

    ы

---
+++ Content.Server/_Sunrise/Entry/SunriseServerEntry.cs
@@ -0,0 +1,15 @@
+using Content.Sunrise.Interfaces.Server;
+using Content.Sunrise.Interfaces.Shared;
+
+namespace Content.Server._Sunrise.Entry;
+
+public sealed class SunriseServerEntry
+{
+    public static void Init()
+    {
+    }
+
+    public static void PostInit()
+    {
+    }
+}
