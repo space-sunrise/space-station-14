30ee084b8cee1e1e0e5d6f8b27ad53ba7c99487f
Vigers Ray <vigersray@gmail.com>
2025-02-11 07:30:21 +0300

    Фикс фелинидов

--- Content.Shared/_Sunrise/Felinid/SharedFelinidSystem.cs
+++ Content.Shared/_Sunrise/Felinid/SharedFelinidSystem.cs
@@ -1,4 +1,6 @@
+using Content.Shared.Inventory.Events;
 using Content.Shared.Item;
+using Robust.Shared.Containers;
 
 namespace Content.Shared._Sunrise.Felinid;
 
@@ -9,6 +11,25 @@ public sealed class SharedFelinidSystem : EntitySystem
         base.Initialize();
 
         SubscribeLocalEvent<FelinidComponent, PickupAttemptEvent>(OnPickupAttempt);
+        SubscribeLocalEvent<FelinidComponent, BeingEquippedAttemptEvent>(OnBeingEquippedAttempt);
+        SubscribeLocalEvent<FelinidComponent, ContainerIsInsertingAttemptEvent>(OnHandEquippedAttempt);
+
+    }
+
+    private void OnHandEquippedAttempt(EntityUid uid, FelinidComponent component, ContainerIsInsertingAttemptEvent args)
+    {
+        if (!HasComp<FelinidComponent>(args.EntityUid))
+            return;
+
+        args.Cancel();
+    }
+
+    private void OnBeingEquippedAttempt(Entity<FelinidComponent> ent, ref BeingEquippedAttemptEvent args)
+    {
+        if (!HasComp<FelinidComponent>(args.EquipTarget))
+            return;
+
+        args.Cancel();
     }
 
     private void OnPickupAttempt(EntityUid uid, FelinidComponent component, PickupAttemptEvent args)
