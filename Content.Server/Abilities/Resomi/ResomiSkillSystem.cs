2fe2d883dbd7beaa74fce5537d9e3bb05e88501e
Rinary <rinary.super@gmail.com>
2025-02-21 22:11:09 +0200

    resomi update

--- Content.Server/Abilities/Resomi/ResomiSkillSystem.cs
+++ Content.Server/Abilities/Resomi/ResomiSkillSystem.cs
@@ -1,87 +1,39 @@
-using Content.Server.Popups;
-using Content.Shared.Actions;
-using Content.Shared.Actions.Events;
-using Content.Shared.Alert;
-using Content.Shared.Coordinates.Helpers;
-using Content.Shared.Maps;
-using Content.Shared.Mobs.Components;
-using Content.Shared.Physics;
-using Robust.Shared.Containers;
-using Robust.Shared.Map;
-using Robust.Shared.Timing;
-using Content.Shared.Speech.Muting;
-using Robust.Shared.Physics.Systems;
+using Content.Server.Actions;
+using Content.Shared.Abilities.Resomi;
+using Content.Shared.Throwing;
+using Robust.Shared.Prototypes;
 
-namespace Content.Server.Abilities.Resomi
+namespace Content.Server.Abilities.Resomi;
+
+public sealed class ResomiSkillSystem : EntitySystem
 {
-    public sealed class ResomiSkillSystem : EntitySystem
+    [Dependency] private readonly ActionsSystem _action = default!;
+    [Dependency] private readonly ThrowingSystem _throwing = default!;
+    [Dependency] private readonly SharedTransformSystem _transform = default!;
+
+    public override void Initialize()
     {
-        [Dependency] private readonly PopupSystem _popupSystem = default!;
-        [Dependency] private readonly SharedActionsSystem _actionsSystem = default!;
-        [Dependency] private readonly AlertsSystem _alertsSystem = default!;
-        [Dependency] private readonly EntityLookupSystem _lookupSystem = default!;
-        [Dependency] private readonly TurfSystem _turf = default!;
-        [Dependency] private readonly IMapManager _mapMan = default!;
-        [Dependency] private readonly SharedContainerSystem _container = default!;
-        [Dependency] private readonly IGameTiming _timing = default!;
+        base.Initialize();
 
-        public override void Initialize()
-        {
-            base.Initialize();
-            SubscribeLocalEvent<ResomiSkillComponent, ComponentInit>(OnComponentInit);
-            SubscribeLocalEvent<ResomiSkillComponent, InvisibleWallActionEvent>(OnInvisibleWall);
-        }
+        SubscribeLocalEvent<ResomiSkillComponent, ComponentStartup>(OnStartup);
+        SubscribeLocalEvent<ResomiSkillComponent, ResomiJumpActionEvent>(OnJump);
+    }
+    
+    private void OnStartup(EntityUid uid, ResomiSkillComponent component, ComponentStartup args) => _action.AddAction(uid, component.ActionJumpId);
+    
+    private void OnJump(EntityUid uid, ResomiSkillComponent component, ResomiJumpActionEvent args)
+    {
+        if (args.Handled)
+            return;
 
-        public override void Update(float frameTime)
-        {
-            base.Update(frameTime);
-            // Queue to track whether mimes can retake vows yet
+        args.Handled = true;
+        var xform = Transform(uid);
+        var mapCoords = args.Target.ToMap(EntityManager, _transform);
+        var direction = mapCoords.Position - xform.MapPosition.Position;
 
-            var query = EntityQueryEnumerator<ResomiSkillComponent>();
-            while (query.MoveNext(out var uid, out var mime))
-            {
-                if (!mime.VowBroken || mime.ReadyToRepent)
-                    continue;
+        if (direction.Length() > component.MaxThrow)
+            direction = direction.Normalized() * component.MaxThrow;
 
-                if (_timing.CurTime < mime.VowRepentTime)
-                    continue;
-
-                mime.ReadyToRepent = true;
-                _popupSystem.PopupEntity(Loc.GetString("mime-ready-to-repent"), uid, uid);
-            }
-        }
-
-        private void OnComponentInit(EntityUid uid, ResomiSkillComponent component, ComponentInit args)
-        {
-            EnsureComp<MutedComponent>(uid);
-            _alertsSystem.ShowAlert(uid, component.VowAlert);
-            _actionsSystem.AddAction(uid, ref component.InvisibleWallActionEntity, component.InvisibleWallAction, uid);
-        }
-
-        /// <summary>
-        /// Creates an invisible wall in a free space after some checks.
-        /// </summary>
-        private void OnInvisibleWall(EntityUid uid, ResomiSkillComponent component, InvisibleWallActionEvent args)
-        {
-            if (!component.Enabled)
-                return;
-
-            if (_container.IsEntityOrParentInContainer(uid))
-                return;
-
-            var xform = Transform(uid);
-            // Get the tile in front of the mime
-            var offsetValue = xform.LocalRotation.ToWorldVec();
-            var coords = xform.Coordinates.Offset(offsetValue).SnapToGrid(EntityManager, _mapMan);
-            var tile = coords.GetTileRef(EntityManager, _mapMan);
-            if (tile == null)
-                return;
-
-            _popupSystem.PopupEntity(Loc.GetString("mime-invisible-wall-popup", ("mime", uid)), uid);
-            // Make sure we set the invisible wall to despawn properly
-            Spawn(component.WallPrototype, _turf.GetTileCenter(tile.Value));
-            // Handle args so cooldown works
-            args.Handled = true;
-        }
+        _throwing.TryThrow(uid, direction, 7F, uid, 10F);
     }
 }
