a91f9e32ce1dfa7470e6b8a5bb009c29f3c67fb5
pacable <igor.mamaev1@gmail.com>
2025-03-08 19:20:24 +0300

    base reward items
    Signed-off-by: pacable <igor.mamaev1@gmail.com>

---
+++ Content.Shared/_Sunrise/Boss/Systems/ApplyAngularVelocitySystem.cs
@@ -0,0 +1,23 @@
+﻿using Content.Shared._Sunrise.Boss.Components;
+using Robust.Shared.Physics.Systems;
+
+namespace Content.Shared._Sunrise.Boss.Systems;
+
+/// <summary>
+/// This handles...
+/// </summary>
+public sealed class ApplyAngularVelocitySystem : EntitySystem
+{
+    [Dependency] private readonly SharedPhysicsSystem _physics = default!;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<ApplyAngularVelocityComponent, ComponentInit>(OnInit);
+    }
+
+    private void OnInit(EntityUid uid, ApplyAngularVelocityComponent component, ComponentInit args)
+    {
+        _physics.ApplyAngularImpulse(uid, component.Impulse);
+    }
+}
