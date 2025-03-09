46e3c427787c9ec39a53c38c5970656e71875193
pacable <igor.mamaev1@gmail.com>
2025-03-09 12:17:35 +0300

    шар пламени
    Signed-off-by: pacable <igor.mamaev1@gmail.com>

---
+++ Content.Shared/_Sunrise/Boss/Components/SpiralMovementComponent.cs
@@ -0,0 +1,22 @@
+﻿using Robust.Shared.Map;
+
+namespace Content.Shared._Sunrise.Boss.Components;
+
+[RegisterComponent]
+public sealed partial class SpiralMovementComponent : Component
+{
+    [DataField]
+    public EntityCoordinates? OriginCoordinates;
+
+    [DataField]
+    public TimeSpan RadiusCoefficient = TimeSpan.FromSeconds(0.3);
+
+    [DataField]
+    public float OmegaCoefficient = 2f * (float)Math.PI;
+
+    [DataField]
+    public float Offset;
+
+    [DataField(readOnly: true)]
+    public TimeSpan? SpawnTime;
+}
