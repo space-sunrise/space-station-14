673410573d99ae2241b408419299f0c03d581bfc
pacable <igor.mamaev1@gmail.com>
2025-03-12 22:20:48 +0300

    Невозможно открутить алтарь
    Signed-off-by: pacable <igor.mamaev1@gmail.com>

---
+++ Content.Shared/_Sunrise/Anchoring/Components/UnanchorableComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace Content.Shared._Sunrise.Anchoring.Components;
+
+/// <summary>
+/// Неоткручиваемые объекты
+/// </summary>
+[RegisterComponent]
+public sealed partial class UnanchorableComponent : Component
+{
+
+}
