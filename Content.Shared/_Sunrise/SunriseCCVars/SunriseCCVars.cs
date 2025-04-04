821537a40c62c9d4c5cd38bc380ed2b4e3e015c7
SplikZerys <tronezero700@yandex.ru>
2025-04-04 11:33:38 +0300

    map corrections

--- Content.Shared/_Sunrise/SunriseCCVars/SunriseCCVars.cs
+++ Content.Shared/_Sunrise/SunriseCCVars/SunriseCCVars.cs
@@ -278,7 +278,7 @@ public sealed class SunriseCCVars
 
     public static readonly CVarDef<int> VotingsDelay = CVarDef.Create("vote.votings_delay", 60);
 
-    public static readonly CVarDef<int> MapVotingCount = CVarDef.Create("vote.map_voting_count", 3);
+    public static readonly CVarDef<int> MapVotingCount = CVarDef.Create("vote.map_voting_count", 5);
 
     public static readonly CVarDef<int> RoundVotingCount = CVarDef.Create("vote.round_voting_count", 3);
 
