8c7a4ffb2d29b40911494bc763d6eae537bd55cd
banumbas <banumbas1@gmail.com>
2025-04-06 21:22:22 +0300

    Понел

--- Content.Server/Humanoid/Components/RandomHumanoidAppearanceComponent.cs
+++ Content.Server/Humanoid/Components/RandomHumanoidAppearanceComponent.cs
@@ -7,14 +7,12 @@ namespace Content.Server.CharacterAppearance.Components;
 [RegisterComponent]
 public sealed partial class RandomHumanoidAppearanceComponent : Component
 {
-    [DataField("randomizeName")] 
-    public bool RandomizeName = true;
+    [DataField("randomizeName")] public bool RandomizeName = true;
 
     /// <summary>
     /// After randomizing, sets the hair style to this, if possible
     /// </summary>
-    [DataField] 
-    public string? Hair = null;
+    [DataField] public string? Hair = null;
 
     // Sunrise-Start
     /// <summary>
--- Content.Server/Humanoid/Systems/RandomHumanoidAppearanceSystem.cs
+++ Content.Server/Humanoid/Systems/RandomHumanoidAppearanceSystem.cs
@@ -7,31 +7,29 @@ namespace Content.Server.Humanoid.Systems;
 
 public sealed class RandomHumanoidAppearanceSystem : EntitySystem
 {
-    [Dependency] 
-    private readonly HumanoidAppearanceSystem _humanoid = default!;
+    [Dependency] private readonly HumanoidAppearanceSystem _humanoid = default!;
     
-    [Dependency] 
-    private readonly MetaDataSystem _metaData = default!;
+    [Dependency] private readonly MetaDataSystem _metaData = default!;
 
     public override void Initialize()
     {
         base.Initialize();
+
         SubscribeLocalEvent<RandomHumanoidAppearanceComponent, MapInitEvent>(OnMapInit);
     }
 
     private void OnMapInit(EntityUid uid, RandomHumanoidAppearanceComponent component, MapInitEvent args)
     {
-        if (!TryComp(uid, out HumanoidAppearanceComponent? humanoid) || 
-            !string.IsNullOrEmpty(humanoid.Initial))
+        if (!TryComp(uid, out HumanoidAppearanceComponent? humanoid) || !string.IsNullOrEmpty(humanoid.Initial))
         {
             return;
         }
 
         var profile = HumanoidCharacterProfile.RandomWithSpecies(humanoid.Species);
-        
+        //If we have a specified hair style, change it to this        
         if (component.Hair != null)
             profile = profile.WithCharacterAppearance(profile.Appearance.WithHairStyleName(component.Hair));
-        
+
         _humanoid.LoadProfile(uid, profile, humanoid);
 
         if (component.RandomizeName)
