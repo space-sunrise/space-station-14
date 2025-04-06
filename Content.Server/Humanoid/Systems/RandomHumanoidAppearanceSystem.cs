c82cfc205e71500ce10bb5a62d7ca1b869eae1e7
banumbas <banumbas1@gmail.com>
2025-04-06 21:32:19 +0300

    ОМГ...

--- Content.Server/Humanoid/Systems/RandomHumanoidAppearanceSystem.cs
+++ Content.Server/Humanoid/Systems/RandomHumanoidAppearanceSystem.cs
@@ -8,7 +8,6 @@ namespace Content.Server.Humanoid.Systems;
 public sealed class RandomHumanoidAppearanceSystem : EntitySystem
 {
     [Dependency] private readonly HumanoidAppearanceSystem _humanoid = default!;
-    
     [Dependency] private readonly MetaDataSystem _metaData = default!;
 
     public override void Initialize()
@@ -20,14 +19,15 @@ public sealed class RandomHumanoidAppearanceSystem : EntitySystem
 
     private void OnMapInit(EntityUid uid, RandomHumanoidAppearanceComponent component, MapInitEvent args)
     {
+        // If we have an initial profile/base layer set, do not randomize this humanoid.
         if (!TryComp(uid, out HumanoidAppearanceComponent? humanoid) || !string.IsNullOrEmpty(humanoid.Initial))
         {
             return;
         }
 
         var profile = HumanoidCharacterProfile.RandomWithSpecies(humanoid.Species);
-        //If we have a specified hair style, change it to this        
-        if (component.Hair != null)
+        //If we have a specified hair style, change it to this
+        if(component.Hair != null)
             profile = profile.WithCharacterAppearance(profile.Appearance.WithHairStyleName(component.Hair));
 
         _humanoid.LoadProfile(uid, profile, humanoid);
