314596880638b524a2dac03b48faa8f48b4fca33
Vigers Ray <vigersray@gmail.com>
2025-04-06 04:49:47 +0300

    фиксы апстрима и активация колизии

--- Content.Shared/Standing/SharedStandingStateSystem.cs
+++ Content.Shared/Standing/SharedStandingStateSystem.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using Content.Shared._Sunrise.Jump;
 using Content.Shared.ActionBlocker;
 using Content.Shared.Buckle;
@@ -6,6 +7,7 @@ using Content.Shared.Damage.Systems;
 using Content.Shared.DoAfter;
 using Content.Shared.Emoting;
 using Content.Shared.Hands.Components;
+using Content.Shared.Hands.EntitySystems;
 using Content.Shared.Mobs;
 using Content.Shared.Mobs.Systems;
 using Content.Shared.Movement.Events;
@@ -19,6 +21,7 @@ using Robust.Shared.Network;
 using Robust.Shared.Physics;
 using Robust.Shared.Physics.Components;
 using Robust.Shared.Physics.Systems;
+using Robust.Shared.Random;
 
 namespace Content.Shared.Standing;
 
@@ -33,9 +36,10 @@ public abstract class SharedStandingStateSystem : EntitySystem
     [Dependency] private readonly MovementSpeedModifierSystem _movement = default!;
     [Dependency] private readonly SharedRotationVisualsSystem _rotation = default!;
     [Dependency] private readonly SharedBuckleSystem _buckle = default!;
-    [Dependency] private readonly StaminaSystem _stamina = default!;
-    [Dependency] private readonly SharedStunSystem _stun = default!;
     [Dependency] private readonly ThrowingSystem _throwing = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly SharedHandsSystem _handsSystem = default!;
+    [Dependency] private readonly SharedTransformSystem _transform = default!;
 
     private const int StandingCollisionLayer = (int) CollisionGroup.MidImpassable;
 
@@ -47,6 +51,50 @@ public abstract class SharedStandingStateSystem : EntitySystem
         SubscribeLocalEvent<StandingStateComponent, DownDoAfterEvent>(OnDownDoAfter);
         SubscribeLocalEvent<StandingStateComponent, MoveEvent>(OnMove);
         SubscribeLocalEvent<StandingStateComponent, RefreshMovementSpeedModifiersEvent>(OnRefreshMovementSpeed);
+        SubscribeLocalEvent<StandingStateComponent, AttemptMobCollideEvent>(OnMobCollide);
+        SubscribeLocalEvent<StandingStateComponent, AttemptMobTargetCollideEvent>(OnMobTargetCollide);
+        SubscribeLocalEvent<StandingStateComponent, DropHandItemsEvent>(FallOver);
+    }
+
+    private void FallOver(EntityUid uid, StandingStateComponent component, DropHandItemsEvent args)
+    {
+        var direction = EntityManager.TryGetComponent(uid, out PhysicsComponent? comp) ? comp.LinearVelocity / 50 : Vector2.Zero;
+        var dropAngle = _random.NextFloat(0.8f, 1.2f);
+
+        if (!TryComp(uid, out HandsComponent? handsComp))
+            return;
+
+        var worldRotation = _transform.GetWorldRotation(uid).ToVec();
+        foreach (var hand in handsComp.Hands.Values)
+        {
+            if (hand.HeldEntity is not { } held)
+                continue;
+            if (!_handsSystem.TryDrop(uid, hand, checkActionBlocker: false, handsComp: handsComp))
+                continue;
+
+            _throwing.TryThrow(
+                held,
+                _random.NextAngle().RotateVec(direction / dropAngle + worldRotation / 50),
+                0.5f * dropAngle * _random.NextFloat(-0.9f, 1.1f),
+                uid,
+                0);
+        }
+    }
+
+    private void OnMobTargetCollide(Entity<StandingStateComponent> ent, ref AttemptMobTargetCollideEvent args)
+    {
+        if (ent.Comp.CurrentState == StandingState.Laying)
+        {
+            args.Cancelled = true;
+        }
+    }
+
+    private void OnMobCollide(Entity<StandingStateComponent> ent, ref AttemptMobCollideEvent args)
+    {
+        if (ent.Comp.CurrentState == StandingState.Laying)
+        {
+            args.Cancelled = true;
+        }
     }
 
     #region Implementation
@@ -146,7 +194,7 @@ public abstract class SharedStandingStateSystem : EntitySystem
         return _doAfter.TryStartDoAfter(args);
     }
 
-    public void Fall(EntityUid uid, float rollDistance = 3f, float rollSpeed = 6f)
+    public void Fall(EntityUid uid, float rollDistance = 6f, float rollSpeed = 6f)
     {
         if (!TryComp<PhysicsComponent>(uid, out var physics))
             return;
