13193b095a58cdffc5b3c34219746ea541316491
Vigers Ray <vigersray@gmail.com>
2025-04-11 04:59:11 +0300

    Removes the option for players to disable recoil their own recoil. (#5220)

--- Content.Client/Options/UI/Tabs/AccessibilityTab.xaml.cs
+++ Content.Client/Options/UI/Tabs/AccessibilityTab.xaml.cs
@@ -15,7 +15,6 @@ public sealed partial class AccessibilityTab : Control
         Control.AddOptionCheckBox(CCVars.ChatEnableColorName, EnableColorNameCheckBox);
         Control.AddOptionCheckBox(CCVars.AccessibilityColorblindFriendly, ColorblindFriendlyCheckBox);
         Control.AddOptionCheckBox(CCVars.ReducedMotion, ReducedMotionCheckBox);
-        Control.AddOptionPercentSlider(CCVars.ScreenShakeIntensity, ScreenShakeIntensitySlider);
         Control.AddOptionPercentSlider(CCVars.ChatWindowOpacity, ChatWindowOpacitySlider);
         Control.AddOptionPercentSlider(CCVars.SpeechBubbleTextOpacity, SpeechBubbleTextOpacitySlider);
         Control.AddOptionPercentSlider(CCVars.SpeechBubbleSpeakerOpacity, SpeechBubbleSpeakerOpacitySlider);
