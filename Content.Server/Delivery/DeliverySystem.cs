87ea7a07ce5bc919ee9f54f3346ed1690305e541
pacable <igor.mamaev1@gmail.com>
2025-04-20 15:50:08 +0300

    Посылки больше не приходят боргам

--- Content.Server/Delivery/DeliverySystem.cs
+++ Content.Server/Delivery/DeliverySystem.cs
@@ -8,6 +8,9 @@ using Content.Shared.Labels.EntitySystems;
 using Content.Shared.StationRecords;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Containers;
+using Robust.Shared.Prototypes; // Sunrise-add
+using System.Diagnostics.CodeAnalysis; // Sunrise-add
+using Content.Shared.Roles; // Sunrise-add
 
 namespace Content.Server.Delivery;
 
@@ -25,6 +28,20 @@ public sealed partial class DeliverySystem : SharedDeliverySystem
     [Dependency] private readonly FingerprintReaderSystem _fingerprintReader = default!;
     [Dependency] private readonly LabelSystem _label = default!;
     [Dependency] private readonly SharedContainerSystem _container = default!;
+    // Sunrise-start
+    [Dependency] private readonly IPrototypeManager _prototypeMan = default!;
+
+    /// <summary>
+    /// Список с департаментами, которым нельзя доставлять посылки
+    /// </summary>
+    private List<string> _bannedDepartmentsRaw = new List<string>()
+    {
+        "Silicon",
+    };
+
+    // Будем использовать хэшсет для ускорения поиска
+    private HashSet< ProtoId<JobPrototype>> _bannedRoles = new HashSet< ProtoId<JobPrototype>>();
+    // Sunrise-end
 
     public override void Initialize()
     {
@@ -33,7 +50,49 @@ public sealed partial class DeliverySystem : SharedDeliverySystem
         SubscribeLocalEvent<DeliveryComponent, MapInitEvent>(OnMapInit);
 
         InitializeSpawning();
+
+        IndexBannedDepartments(); // Sunrise-add
     }
+
+    // Sunrise-start
+    /// <summary>
+    /// Индексируем работы во всех департаментах, которым мы запретили отправлять посылки
+    /// Вызывается 1 раз
+    /// </summary>
+    private void IndexBannedDepartments()
+    {
+        foreach (var bannedDepartment in _bannedDepartmentsRaw)
+        {
+            if (!_prototypeMan.TryIndex<DepartmentPrototype>(bannedDepartment, out var prototype))
+                continue;
+            foreach (var role in prototype.Roles)
+            {
+                _bannedRoles.Add(role);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Название говорит само за себя, но эта функция пытается найти валидную цель для доставки посылки
+    /// </summary>
+    /// <param name="stationId">Станция, по которой проводить поиск</param>
+    /// <param name="record">Профиль найденной цели. Null если не найдена корректная цель</param>
+    /// <returns></returns>
+    private bool TryGetUnbannedRecord(EntityUid stationId, [NotNullWhen(true)] out GeneralStationRecord? record)
+    {
+        var attempts = 0;
+        while (_records.TryGetRandomRecord(stationId, out record) &&
+               record != null &&
+               _bannedRoles.Contains(record.JobPrototype))
+        {
+            attempts++;
+            if (attempts >= 10)
+                return false;
+        }
+
+        return record != null;
+    }
+    // Sunrise-end
 
     private void OnMapInit(Entity<DeliveryComponent> ent, ref MapInitEvent args)
     {
@@ -44,9 +103,13 @@ public sealed partial class DeliverySystem : SharedDeliverySystem
         if (stationId == null)
             return;
 
-        _records.TryGetRandomRecord<GeneralStationRecord>(stationId.Value, out var entry);
-
-        if (entry == null)
+        // Sunrise-start
+        // _records.TryGetRandomRecord<GeneralStationRecord>(stationId.Value, out var entry);
+        //
+        // if (entry == null)
+        //     return;
+        if (!TryGetUnbannedRecord(stationId.Value, out var entry))
+        // Sunrise-end
             return;
 
         ent.Comp.RecipientName = entry.Name;
