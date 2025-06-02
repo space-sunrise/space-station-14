8cef0227626ba96344dd445cbf60377aba3dc141
iDesmond <kdakdo2014@gmail.com>
2025-06-02 18:33:44 +0300

    edits

--- Content.Server/_Sunrise/Chat/AntiSpamSystem.cs
+++ Content.Server/_Sunrise/Chat/AntiSpamSystem.cs
@@ -15,18 +15,21 @@ public sealed class AntiSpamSystem : EntitySystem
     [Dependency] private readonly SharedPopupSystem _popup = default!;
     [Dependency] private readonly IGameTiming _timing = default!;
     [Dependency] private readonly StatusEffectsSystem _statusEffects = default!;
-    private readonly Dictionary<NetUserId, List<(string Message, float Time)>> _messageHistory = new();
-    private const float SpamWindow = 3f;
-    private const int MaxSameMessageCount = 3;
-    private const int MaxPlayerMessageHistorySize = 6;
+
+    private static readonly Dictionary<NetUserId, List<(string Message, float Time)>> MessageHistory = new();
+
+    private const int CounterShort = 1; // allowed number of messages for TimeShort duration
+    private const int CounterLong = 2; // allowed number of messages for TimeLong duration
+    private const int MuteDuration = 300; // mute time
+    private const float TimeShort = 1.5f; // minimum check time
+    private const float TimeLong = 5f; // maximum check time
 
     public override void Initialize()
     {
         SubscribeLocalEvent<MobStateComponent, TrySendICMessageEvent>(SpamICCheck);
     }
 
-
-    private void SpamICCheck(EntityUid uid, MobStateComponent component, ref TrySendICMessageEvent args)
+    private void SpamICCheck(Entity<MobStateComponent> ent, ref TrySendICMessageEvent args)
     {
         if (args.Player == null)
             return;
@@ -36,40 +39,34 @@ public sealed class AntiSpamSystem : EntitySystem
 
         var now = (float)_timing.CurTime.TotalSeconds;
 
-        if (!_messageHistory.TryGetValue(args.Player.UserId, out var history))
+        if (!MessageHistory.TryGetValue(args.Player.UserId, out var history))
         {
-            // список с предложенной емкостью, чтобы уменьшить переаллокации.
             history = new List<(string Message, float Time)>();
-            _messageHistory[args.Player.UserId] = history;
+            MessageHistory[args.Player.UserId] = history;
         }
 
         // Cleaning up old records (older than 5 seconds)
-        history.RemoveAll(m => now - m.Time > 5f);
+        history.RemoveAll(m => now - m.Time > TimeLong);
         var currentMessage = args.Message;
 
         // Add current message
         history.Add((currentMessage, now));
 
-
-
         // Count repetitions for the last 1.5 and 5 seconds
-        int repeatsInShort = history.Count(m => m.Message == currentMessage && now - m.Time <= 1.5f);
-        int repeatsInLong = history.Count(m => m.Message == currentMessage);
+        var repeatsInShort = history.Count(m => m.Message == currentMessage && now - m.Time <= TimeShort);
+        var repeatsInLong = history.Count(m => m.Message == currentMessage);
 
-        if (repeatsInShort > 1 || repeatsInLong > 2)
+        if (repeatsInShort > CounterShort || repeatsInLong > CounterLong)
         {
             history.Clear(); // reset spam history
             args.Cancel();
 
             var selfMessage = Loc.GetString("spam-mute-text-self");
-            _popup.PopupEntity(selfMessage, uid, PopupType.Large);
+            _popup.PopupEntity(selfMessage, ent, PopupType.Large);
 
-            _statusEffects.TryAddStatusEffect<MutedComponent>(uid, "Muted", TimeSpan.FromSeconds(300), true);
-
-            return;
+            _statusEffects.TryAddStatusEffect<MutedComponent>(ent, "Muted", TimeSpan.FromSeconds(MuteDuration), true);
         }
     }
-
 }
 
 public sealed class TrySendICMessageEvent(string message, InGameICChatType desiredType, ICommonSession? player = null) : CancellableEntityEventArgs
