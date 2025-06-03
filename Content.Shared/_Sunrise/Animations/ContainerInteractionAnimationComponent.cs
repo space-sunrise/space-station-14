0c74255c0626dd4524338085f3a1cdf9193c1086
drdth <drdtheuser@gmail.com>
2025-06-03 04:31:16 +0300

    tweak: increase variation

--- Content.Shared/_Sunrise/Animations/ContainerInteractionAnimationComponent.cs
+++ Content.Shared/_Sunrise/Animations/ContainerInteractionAnimationComponent.cs
@@ -5,6 +5,6 @@ namespace Content.Shared._Sunrise.Animations;
 [RegisterComponent, NetworkedComponent]
 public sealed partial class ContainerInteractionAnimationComponent : Component
 {
-    [DataField, ViewVariables] public float Variation = 0.1f;
+    [DataField, ViewVariables] public float Variation = 0.2f;
     [DataField, ViewVariables] public float Duration = 0.2f;
 }
