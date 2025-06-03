c247bfab803a22ff54f328d53355b6be72835bc2
drdth <drdtheuser@gmail.com>
2025-06-03 04:14:31 +0300

    add: nice animation for storages

---
+++ Content.Shared/_Sunrise/Animations/ContainerInteractionAnimationComponent.cs
@@ -0,0 +1,13 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._Sunrise.Animations;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class ContainerInteractionAnimationComponent : Component
+{
+    private const float AnimationDuration = 0.2f;
+    public const float Variation = 0.1f;
+
+    [DataField]
+    public float Duration = AnimationDuration;
+}
