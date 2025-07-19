8687a726e98e07f688df7222e72d1a526c033596
banumbas <banumbas1@gmail.com>
2025-07-20 00:25:19 +0300

    Revert "Шейдер мету"
    This reverts commit 98d07f731d1f6279f2f8773b5a12dc0833487bca.

--- Content.Shared/_Sunrise/Drugs/SeeingFractalRainbowsComponent.cs
+++
@@ -1,6 +0,0 @@
-using Robust.Shared.GameStates;
-
-namespace Content.Shared.Drugs;
-
-[RegisterComponent, NetworkedComponent]
-public sealed partial class SeeingFractalRainbowsComponent : Component { }
