94b50d92255daf406ae7f2aa4e03b49b1a39768a
Ligyb <ligybs@gmail.com>
2025-07-27 02:27:11 +0500

    фикс отображения градиентов у разных бодитайпов, НО

--- Content.Client/Humanoid/HumanoidAppearanceSystem.cs
+++ Content.Client/Humanoid/HumanoidAppearanceSystem.cs
@@ -423,7 +423,11 @@ public sealed class HumanoidAppearanceSystem : SharedHumanoidAppearanceSystem
                 instance.ApplyShaderParams(markingEffects[j], new Vector2(texWidth, texHeight));
 
                 sprite.LayerSetShader(layerId, instance);
-                _sprite.LayerSetColor((entity.Owner, sprite), layerId, Color.White);
+                _sprite.LayerSetColor(
+                    (entity.Owner, sprite),
+                    layerId,
+                    markingEffects[j].Colors.TryGetValue("base", out var col) ? col : Color.White
+                    );
             }
             else
             {
