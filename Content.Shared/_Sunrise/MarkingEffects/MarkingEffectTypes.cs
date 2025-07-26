cc400beb69c070cb656a966049c8cc14adf3cb2a
Ligyb <ligybs@gmail.com>
2025-07-26 18:35:44 +0500

    нормальное разделение классов

---
+++ Content.Shared/_Sunrise/MarkingEffects/MarkingEffectTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace Content.Shared._Sunrise.MarkingEffects;
+
+// какой-то прям костыль, надо как-то по-другому парсер реализовать
+public static class MarkingEffectTypes
+{
+    public static readonly Dictionary<MarkingEffectType, Func<Dictionary<string, string>, MarkingEffect?>> TypeParsers = new()
+    {
+        { MarkingEffectType.Color, ColorMarkingEffect.Parse },
+        { MarkingEffectType.Gradient, GradientMarkingEffect.Parse }
+    };
+}
