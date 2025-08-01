e5c94a49a0089547e5ced003a29b4e830621b76b
A-Mironov <65418470+Alexnov33X@users.noreply.github.com>
2025-08-01 11:13:02 +0300

    Оно не регается

--- Content.Server/_Sunrise/StationEvents/Components/EpsilonDeathSquadLawsetComponent.cs
+++ Content.Server/_Sunrise/StationEvents/Components/EpsilonDeathSquadLawsetComponent.cs
@@ -7,8 +7,7 @@ namespace Content.Server._Sunrise.StationEvents.Components;
 /// Component for the Epsilon Death Squad Lawset event.
 /// Stores the target station where the event should affect borgs.
 /// </summary>
-/*
-[RegisterComponent, NetworkedComponent, ]
+[RegisterComponent, NetworkedComponent, Access(typeof(EpsilonDeathSquadLawsetRule))]
 public sealed partial class EpsilonDeathSquadLawsetComponent : Component
 {
     /// <summary>
@@ -17,4 +16,3 @@ public sealed partial class EpsilonDeathSquadLawsetComponent : Component
     [DataField]
     public EntityUid TargetStation = EntityUid.Invalid;
 }
-*/
