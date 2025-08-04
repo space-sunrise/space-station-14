2534fede540eab8c8401f2baac8df645f67e9604
drdth <drdtheuser@gmail.com>
2025-08-04 19:56:24 +0300

    tweak: players are blind in tubes now

---
+++ Content.Server/_Sunrise/Misc/BlindInDisposals/BlindInDisposalsSystem.cs
@@ -0,0 +1,35 @@
+﻿using Content.Server.Disposal.Unit;
+using Content.Shared.Eye.Blinding.Components;
+
+namespace Content.Server._Sunrise.Misc.BlindInDisposals;
+
+/// <summary>
+/// Простая система, делающая персонажа слепым, пока он находится в трубах.
+/// Из труб не должно быть видно реальный мир!!
+/// </summary>
+public sealed class BlindInDisposalsSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<BeingDisposedComponent, ComponentStartup>(OnStartup);
+        SubscribeLocalEvent<BeingDisposedComponent, ComponentShutdown>(OnShutdown);
+    }
+
+    private void OnStartup(Entity<BeingDisposedComponent> ent, ref ComponentStartup args)
+    {
+        if (!HasComp<BlindableComponent>(ent))
+            return;
+
+        EnsureComp<TemporaryBlindnessComponent>(ent);
+    }
+
+    private void OnShutdown(Entity<BeingDisposedComponent> ent, ref ComponentShutdown args)
+    {
+        if (!HasComp<BlindableComponent>(ent))
+            return;
+
+        RemComp<TemporaryBlindnessComponent>(ent);
+    }
+}
