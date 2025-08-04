46d817ca7356e0a473d8dd409d90078dd8c2c06d
drdth <drdtheuser@gmail.com>
2025-08-04 20:10:48 +0300

    refactor: better implementation

--- Content.Server/_Sunrise/Misc/BlindInDisposals/BlindInDisposalsSystem.cs
+++ Content.Server/_Sunrise/Misc/BlindInDisposals/BlindInDisposalsSystem.cs
@@ -1,5 +1,6 @@
 ﻿using Content.Server.Disposal.Unit;
 using Content.Shared.Eye.Blinding.Components;
+using Content.Shared.Eye.Blinding.Systems;
 
 namespace Content.Server._Sunrise.Misc.BlindInDisposals;
 
@@ -9,27 +10,40 @@ namespace Content.Server._Sunrise.Misc.BlindInDisposals;
 /// </summary>
 public sealed class BlindInDisposalsSystem : EntitySystem
 {
+    [Dependency] private readonly BlindableSystem _blindable = default!;
+
     public override void Initialize()
     {
         base.Initialize();
 
         SubscribeLocalEvent<BeingDisposedComponent, ComponentStartup>(OnStartup);
-        SubscribeLocalEvent<BeingDisposedComponent, ComponentShutdown>(OnShutdown);
+        SubscribeLocalEvent<BeingDisposedComponent, ComponentRemove>(OnRemove);
+        SubscribeLocalEvent<BeingDisposedComponent, CanSeeAttemptEvent>(OnCanSee);
     }
 
     private void OnStartup(Entity<BeingDisposedComponent> ent, ref ComponentStartup args)
     {
+        // Это здесь, так как АПИ тела вызывает NRE при отсутствии глаз у выбранного ентити. Крутая система
+        // TODO: Портировать фикс NRE с фаера
         if (!HasComp<BlindableComponent>(ent))
             return;
 
-        EnsureComp<TemporaryBlindnessComponent>(ent);
+        _blindable.UpdateIsBlind(ent.Owner);
     }
 
-    private void OnShutdown(Entity<BeingDisposedComponent> ent, ref ComponentShutdown args)
+    private void OnRemove(Entity<BeingDisposedComponent> ent, ref ComponentRemove args)
     {
         if (!HasComp<BlindableComponent>(ent))
             return;
 
-        RemComp<TemporaryBlindnessComponent>(ent);
+        _blindable.UpdateIsBlind(ent.Owner);
+    }
+
+    private void OnCanSee(Entity<BeingDisposedComponent> ent, ref CanSeeAttemptEvent args)
+    {
+        if (!HasComp<BlindableComponent>(ent))
+            return;
+
+        args.Cancel();
     }
 }
