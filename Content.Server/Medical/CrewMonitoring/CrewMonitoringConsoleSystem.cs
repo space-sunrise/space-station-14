7f73f1da548c81bb2e692721a1a828cef4bc95a7
Vitaliy_Merzun <dunoquas487@gmail.com>
2025-08-28 21:59:26 +0300

    ToggleVerb1

--- Content.Server/Medical/CrewMonitoring/CrewMonitoringConsoleSystem.cs
+++ Content.Server/Medical/CrewMonitoring/CrewMonitoringConsoleSystem.cs
@@ -10,13 +10,17 @@ using Content.Shared.Medical.CrewMonitoring;
 using Content.Shared.Medical.SuitSensor;
 using Content.Shared.Morgue.Components;
 using Content.Shared.Pinpointer;
+using Content.Server.Power.EntitySystems;//Sunrise-Edit
+using Content.Shared.Power.Components;//Sunrise-Edit
+using Content.Shared.PowerCell;//Sunrise-Edit
+using Content.Shared.UserInterface;//Sunrise-Edit
 using Content.Shared.Storage.Components;
-using Content.Shared.Verbs;
+using Content.Shared.Verbs;//Sunrise-Edit
 using Robust.Server.GameObjects;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Map;
 using Robust.Shared.Timing;
-using Robust.Shared.Utility;
+using Robust.Shared.Utility;//Sunrise-Edit
 
 namespace Content.Server.Medical.CrewMonitoring;
 
@@ -33,7 +37,7 @@ public sealed class CrewMonitoringConsoleSystem : EntitySystem
         SubscribeLocalEvent<CrewMonitoringConsoleComponent, ComponentRemove>(OnRemove);
         SubscribeLocalEvent<CrewMonitoringConsoleComponent, DeviceNetworkPacketEvent>(OnPacketReceived);
         SubscribeLocalEvent<CrewMonitoringConsoleComponent, BoundUIOpenedEvent>(OnUIOpened);
-        SubscribeLocalEvent<CrewMonitoringConsoleComponent, GetVerbsEvent<InteractionVerb>>(AddToggleVerb);
+        SubscribeLocalEvent<CrewMonitoringConsoleComponent, GetVerbsEvent<InteractionVerb>>(AddToggleVerb);//Sunrise-Edit
     }
 
     public override void Update(float frameTime)
@@ -54,7 +58,20 @@ public sealed class CrewMonitoringConsoleSystem : EntitySystem
             // Check for corpses with sensors outside morgues
             if (HasCorpsesOutsideMorgue(component))
             {
-                _audio.PlayPvs(component.CorpseAlertSound, uid);
+                if (HasComp<ActivatableUIRequiresPowerCellComponent>(uid) && TryComp<PowerCellDrawComponent>(uid, out var draw))
+                {
+                    if (_cell.HasActivatableCharge(uid, draw))
+                    {
+                        _audio.PlayPvs(component.CorpseAlertSound, uid);
+                    }
+                }
+                if (HasComp<ActivatableUIRequiresPowerComponent>(uid))
+                {
+                    if (this.IsPowered(uid, EntityManager))
+                    {
+                        _audio.PlayPvs(component.CorpseAlertSound, uid);
+                    }
+                }
             }
         }
     }
@@ -161,7 +178,7 @@ public sealed class CrewMonitoringConsoleSystem : EntitySystem
 
         return false;
     }
-
+    //Sunrise-Start
     private void AddToggleVerb(EntityUid uid, CrewMonitoringConsoleComponent component, GetVerbsEvent<InteractionVerb> args)
     {
         if (!args.CanInteract || !args.CanAccess)
@@ -176,11 +193,11 @@ public sealed class CrewMonitoringConsoleSystem : EntitySystem
         {
             verb.Text = Loc.GetString("item-toggle-activate-alert");
         }
-        verb.Act = () => ToggleAlert(component);
+        verb.Act = () => ToggleAlert(uid, component);
         args.Verbs.Add(verb);
     }
 
-    public void ToggleAlert(CrewMonitoringConsoleComponent component)
+    public void ToggleAlert(EntityUid uid, CrewMonitoringConsoleComponent component)
     {
         if (component.DoCorpseAlert)
         {
@@ -190,6 +207,7 @@ public sealed class CrewMonitoringConsoleSystem : EntitySystem
         {
             component.DoCorpseAlert = true;
         }
+        Dirty(uid, component);
     }
-
+    //Sunrise-End
 }
