19119391011c08d0f2d572f980d5da92fbdcb4b3
Vigers Ray <vigersray@gmail.com>
2025-09-09 22:06:08 +0300

    а зачем команда когда есть кнопка?

--- Content.Server/Administration/Commands/AdminWhoCommand.cs
+++ Content.Server/Administration/Commands/AdminWhoCommand.cs
@@ -7,7 +7,7 @@ using Robust.Shared.Utility;
 
 namespace Content.Server.Administration.Commands;
 
-[AnyCommand]
+[AdminCommand(AdminFlags.AdminWho)]
 public sealed class AdminWhoCommand : LocalizedCommands
 {
     [Dependency] private readonly IAfkManager _afkManager = default!;
