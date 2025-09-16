f30c5b0b8b6af70041ec27e847fd123118c27704
banumbas <banumbas1@gmail.com>
2025-09-16 11:01:15 +0300

    рудимент

--- Content.Shared/_Sunrise/Tracker/TrackerAlertEvent.cs
+++
@@ -1,6 +0,0 @@
-using Content.Shared.Alert;
-
-namespace Content.Shared._Sunrise.Tracker;
-
-public sealed partial class TrackerClickedAlertEvent : BaseAlertEvent;
-public sealed partial class TrackerAltClickedAlertEvent : BaseAlertEvent;
