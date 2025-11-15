274e34cad2429e2c95ca024f5d087e97edf43512
banumbas <banumbas1@gmail.com>
2025-11-15 13:10:07 +0300

    Мелкое ревью

--- Content.Shared/Ensnaring/SharedEnsnareableSystem.cs
+++ Content.Shared/Ensnaring/SharedEnsnareableSystem.cs
@@ -33,7 +33,7 @@ public abstract class SharedEnsnareableSystem : EntitySystem
     [Dependency] private   readonly SharedHandsSystem _hands = default!;
     [Dependency] protected readonly SharedPopupSystem Popup = default!;
     [Dependency] private   readonly SharedStaminaSystem _stamina = default!;
-    [Dependency] private readonly IEntityManager _entityManager = default!; // Sunrise-Edit
+    [Dependency] private   readonly IEntityManager _entityManager = default!; // Sunrise-Edit
 
     public override void Initialize()
     {
