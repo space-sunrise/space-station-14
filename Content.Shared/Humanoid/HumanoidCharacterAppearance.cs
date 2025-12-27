c1eb00ef8bd24a70db8f46e718755c023c740376
Daniel | Orvex07 <d.b.orvex@gmail.com>
2025-12-27 15:04:08 +0100

    Fix random hair/beard randomization

--- Content.Shared/Humanoid/HumanoidCharacterAppearance.cs
+++ Content.Shared/Humanoid/HumanoidCharacterAppearance.cs
@@ -493,6 +493,14 @@ public sealed partial class HumanoidCharacterAppearance : ICharacterAppearance, 
         var newHairStyle = HairStyles.DefaultHairStyle;
         List<Marking> newMarkings = [];
 
+        var hairStyles = markingManager.MarkingsByCategoryAndSpeciesAndSex(MarkingCategories.Hair, species, sex);
+        if (hairStyles.Count > 0)
+            newHairStyle = random.Pick(hairStyles.Keys.ToArray());
+
+        var facialHairStyles = markingManager.MarkingsByCategoryAndSpeciesAndSex(MarkingCategories.FacialHair, species, sex);
+        if (facialHairStyles.Count > 0)
+            newFacialHairStyle = random.Pick(facialHairStyles.Keys.ToArray());
+
         // grab a completely random color.
         var baseColor = new Color(random.NextFloat(1), random.NextFloat(1), random.NextFloat(1), 1);
 
