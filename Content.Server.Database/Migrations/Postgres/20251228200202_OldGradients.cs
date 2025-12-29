88b46e98073a2d06361c935cec81696d83ab8fb5
drdth <drdtheuser@gmail.com>
2025-12-29 17:48:05 +0300

    test fix

--- Content.Server.Database/Migrations/Postgres/20251228200202_OldGradients.cs
+++ Content.Server.Database/Migrations/Postgres/20251228200202_OldGradients.cs
@@ -10,13 +10,53 @@ namespace Content.Server.Database.Migrations.Postgres
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.AddColumn<int>(
+                name: "facial_hair_color_type",
+                table: "profile",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
 
+            migrationBuilder.AddColumn<string>(
+                name: "facial_hair_extended_color",
+                table: "profile",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "hair_color_type",
+                table: "profile",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "hair_extended_color",
+                table: "profile",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropColumn(
+                name: "facial_hair_color_type",
+                table: "profile");
 
+            migrationBuilder.DropColumn(
+                name: "facial_hair_extended_color",
+                table: "profile");
+
+            migrationBuilder.DropColumn(
+                name: "hair_color_type",
+                table: "profile");
+
+            migrationBuilder.DropColumn(
+                name: "hair_extended_color",
+                table: "profile");
         }
     }
 }
