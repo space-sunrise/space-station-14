8c17ce39440eb87ba2fcf3adaa42bb00a97ca089
CrazyPhantom779 <izayussedwards@gmail.com>
2025-12-29 22:25:14 +1100

    RPD Localization fix

--- Content.Client/RCD/RCDMenuBoundUserInterface.cs
+++ Content.Client/RCD/RCDMenuBoundUserInterface.cs
@@ -24,6 +24,15 @@ public sealed class RCDMenuBoundUserInterface : BoundUserInterface
             ["Airlocks"] = ("rcd-component-airlocks", new SpriteSpecifier.Texture(new ResPath("/Textures/Interface/Radial/RCD/airlocks.png"))),
             ["Electrical"] = ("rcd-component-electrical", new SpriteSpecifier.Texture(new ResPath("/Textures/Interface/Radial/RCD/multicoil.png"))),
             ["Lighting"] = ("rcd-component-lighting", new SpriteSpecifier.Texture(new ResPath("/Textures/Interface/Radial/RCD/lighting.png"))),
+<<<<<<< HEAD
+=======
+            // Starlight start: RPD
+            ["Piping"] = ("rpd-component-piping", new SpriteSpecifier.Texture(new ResPath("/Textures/_Starlight/Interface/Radial/RPD/fourway.png"))),
+            ["AtmosphericUtility"] = ("rpd-component-atmosphericutility", new SpriteSpecifier.Texture(new ResPath("/Textures/_Starlight/Interface/Radial/RPD/port.png"))),
+            ["PumpsValves"] = ("rpd-component-pumpsvalves", new SpriteSpecifier.Texture(new ResPath("/Textures/_Starlight/Interface/Radial/RPD/pump_volume.png"))),
+            ["Vents"] = ("rpd-component-vents", new SpriteSpecifier.Texture(new ResPath("/Textures/_Starlight/Interface/Radial/RPD/vent_passive.png"))),
+            // Starlight end
+>>>>>>> 6dc782d4bd (RPD Localization fix)
         };
 
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
---
+++ Resources/Locale/en-US/_Starlight/rpd/rpd-components
@@ -0,0 +1,4 @@
+rpd-component-piping = Piping
+rpd-component-atmosphericutility = Atmospheric Utility
+rpd-component-pumps = Pumps & Valves
+rpd-component-vents = Vents
