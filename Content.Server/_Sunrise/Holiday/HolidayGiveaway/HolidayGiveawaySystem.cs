80e44f0ba5a970c02176740347693f0f6dc0fe17
drdth <drdtheuser@gmail.com>
2025-12-30 18:44:56 +0300

    refactor: holiday giveaway system to prevent lag spikes on player spawning + fix ultra legacy code

---
+++ Content.Server/_Sunrise/Holiday/HolidayGiveaway/HolidayGiveawaySystem.cs
@@ -0,0 +1,128 @@
+﻿using Content.Server.GameTicking;
+using Content.Server.Hands.Systems;
+using Content.Server.Holiday;
+using Content.Shared.CCVar;
+using Content.Shared.GameTicking;
+using Robust.Shared.Configuration;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server._Sunrise.Holiday.HolidayGiveaway;
+
+/// <summary>
+/// Система для выдачи различных подарков во время определенных праздников.
+/// </summary>
+public sealed class HolidayGiveawaySystem : EntitySystem
+{
+    [Dependency] private readonly IPrototypeManager _prototype = default!;
+    [Dependency] private readonly IConfigurationManager _configuration = default!;
+    [Dependency] private readonly HolidaySystem _holiday = default!;
+    [Dependency] private readonly HandsSystem _hands = default!;
+
+    /// <summary>
+    /// Кешированные текущие раздачи, которые будут применены после спавна игрока в <see cref="OnPlayerSpawn"/> <br/>
+    /// Сбрасываются и пересчитываются каждый раунд в <see cref="CacheGiveaways()"/>
+    /// </summary>
+    private readonly List<ProtoId<HolidayGiveawayItemPrototype>> _activeGiveaways = [];
+
+    [ViewVariables]
+    private bool _enabled = true;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        Subs.CVar(_configuration, CCVars.HolidaysEnabled, OnHolidaysEnableChange);
+
+        SubscribeLocalEvent<GameRunLevelChangedEvent>(CacheGiveaways);
+        SubscribeLocalEvent<RoundStartedEvent>(CacheGiveaways);
+
+        SubscribeLocalEvent<PlayerSpawnCompleteEvent>(OnPlayerSpawn);
+        SubscribeLocalEvent<PreventHolidayGiveawayComponent, HolidayGiveawayAttemptEvent>(Cancel);
+    }
+
+    /// <summary>
+    /// Основной метод для кеширования подарков.
+    /// </summary>
+    private void CacheGiveaways(GameRunLevelChangedEvent ev)
+    {
+        if (!_enabled)
+            return;
+
+        if (ev.New != GameRunLevel.PreRoundLobby)
+            return;
+
+        CacheGiveaways();
+    }
+
+    /// <summary>
+    /// Дополнительный метод для кеширования подарков.
+    /// Специально для случая, когда лобби недоступно или выключено.
+    /// </summary>
+    private void CacheGiveaways(RoundStartedEvent ev)
+    {
+        if (!_enabled)
+            return;
+
+        if (_activeGiveaways.Count != 0)
+            return;
+
+        CacheGiveaways();
+    }
+
+    private void CacheGiveaways()
+    {
+        _activeGiveaways.Clear();
+
+        foreach (var giveaway in _prototype.EnumeratePrototypes<HolidayGiveawayItemPrototype>())
+        {
+            if (!_holiday.IsCurrentlyHoliday(giveaway.Holiday))
+                continue;
+
+            _activeGiveaways.Add(giveaway.ID);
+        }
+    }
+
+    private void OnPlayerSpawn(PlayerSpawnCompleteEvent ev)
+    {
+        if (!_enabled)
+            return;
+
+        if (_activeGiveaways.Count == 0)
+            return;
+
+        var attempt = new HolidayGiveawayAttemptEvent();
+        RaiseLocalEvent(ev.Mob, ref attempt);
+
+        if (attempt.Canceled)
+            return;
+
+        foreach (var giveawayProto in _activeGiveaways)
+        {
+            var giveAway = _prototype.Index(giveawayProto);
+            var present = SpawnNextToOrDrop(giveAway.Prototype, ev.Mob);
+
+            _hands.PickupOrDrop(ev.Mob, present);
+        }
+    }
+
+    private void Cancel(Entity<PreventHolidayGiveawayComponent> ent, ref HolidayGiveawayAttemptEvent args)
+    {
+        args.Canceled = true;
+    }
+
+    private void OnHolidaysEnableChange(bool enabled)
+    {
+        _enabled = enabled;
+
+        if (enabled)
+            CacheGiveaways();
+        else
+            _activeGiveaways.Clear();
+    }
+}
+
+[ByRefEvent]
+public record struct HolidayGiveawayAttemptEvent
+{
+    public bool Canceled;
+}
