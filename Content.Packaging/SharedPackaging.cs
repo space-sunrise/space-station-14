9f3c26d7b4ba20ee1c7f13dbd1f589d6958986d4
Vigers Ray <vigersray@gmail.com>
2026-01-06 05:54:42 +0100

    Объединены ресурсы, игнорируемые при упаковке клиента, для улучшения управления ресурсами

--- Content.Packaging/SharedPackaging.cs
+++ Content.Packaging/SharedPackaging.cs
@@ -2,9 +2,22 @@
 
 public sealed class SharedPackaging
 {
+    /// <summary>
+    /// Resources ignored for both client and server packaging.
+    /// </summary>
     public static readonly IReadOnlySet<string> AdditionalIgnoredResources = new HashSet<string>
     {
         // MapRenderer outputs into Resources. Avoid these getting included in packaging.
         "MapImages",
+    };
+
+    /// <summary>
+    /// Resources ignored only for client packaging (but included in server).
+    /// These resources are loaded dynamically from server to client.
+    /// </summary>
+    public static readonly IReadOnlySet<string> ClientOnlyIgnoredResources = new HashSet<string>
+    {
+        // All _Sunrise resources are loaded dynamically from server
+        "_Sunrise",
     };
 }
