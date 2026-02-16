9d3ab86a06bcdda963cd789d622a0c39d7cf4b02
KaiserMaus <kaiser.ratte@gmail.com>
2026-02-16 14:58:07 +0000

    Squad! Go go go!!!

---
+++ Content.Shared/_Sunrise/NPC/PirateVeteranFollowerComponent.cs
@@ -0,0 +1,18 @@
+using Robust.Shared.GameObjects;
+using Robust.Shared.Serialization.Manager.Attributes;
+
+namespace Content.Shared._Sunrise.NPC;
+
+[RegisterComponent]
+public sealed partial class PirateVeteranFollowerComponent : Component
+{
+    [DataField]
+    public float SearchRadius = 35f;
+
+    [DataField]
+    public float RecheckCooldown = 30f;
+
+    [DataField]
+    public float RecheckAccumulator;
+}
+
