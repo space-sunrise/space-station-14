84ad833340245bba5acf60d945bed55b71c4dc6a
KaiserMaus <kaiser.ratte@gmail.com>
2026-02-23 11:12:34 +0000

    Update Content.Shared/Salvage/Expeditions/Modifiers/SalvageAirMod.cs
    Co-authored-by: Daniel <77834935+Orvex07@users.noreply.github.com>

--- Content.Shared/Salvage/Expeditions/Modifiers/SalvageAirMod.cs
+++ Content.Shared/Salvage/Expeditions/Modifiers/SalvageAirMod.cs
@@ -28,7 +28,7 @@ public sealed partial class SalvageAirMod : IPrototype, IBiomeSpecificMod
     [DataField]
     public List<ProtoId<SalvageBiomeModPrototype>>? Biomes { get; private set; } = null;
 
-    [DataField("difficulties", customTypeSerializer: typeof(PrototypeIdListSerializer<SalvageDifficultyPrototype>))]
+    [DataField(customTypeSerializer: typeof(PrototypeIdListSerializer<SalvageDifficultyPrototype>))]
     public List<string>? Difficulties { get; private set; } = null; // Sunrise-Edit
 
     /// <summary>
