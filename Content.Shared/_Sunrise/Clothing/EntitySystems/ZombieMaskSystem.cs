a810f9d323847bb26ac64d270b0cc54d389bc3a1
bzzzbzzbz <ugorelova.su@gmail.com>
2026-03-20 12:40:30 +0700

    сделал по рекомендации кролика, но не помогло всё равно

--- Content.Shared/_Sunrise/Clothing/EntitySystems/ZombieMaskSystem.cs
+++ Content.Shared/_Sunrise/Clothing/EntitySystems/ZombieMaskSystem.cs
@@ -5,11 +5,18 @@ using Content.Shared.Sunrise.Clothing.EntitySystems;
 using Content.Shared.Sunrise.Clothing.Components;
 using Content.Shared.Clothing;
 using Content.Shared.NameModifier.EntitySystems;
+using Content.Shared.Interaction.Components;
+using Content.Shared.Database;
 
 namespace Content.Shared.Sunrise.Clothing.EntitySystems;
 
 public sealed class ZombieMaskSystem : EntitySystem
 {
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<ZombieMaskComponent, RefreshNameModifiersEvent>(OnRefreshNameModifiers);
+    }
     private void OnRefreshNameModifiers(Entity<ZombieMaskComponent> entity, ref RefreshNameModifiersEvent args)
     {
         args.AddModifier("equipped-name-prefix");
