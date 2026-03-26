4b0b4ff801debd16122dd6f5b39fd4703f522ceb
be quieti <be quieti@DESKTOP-2H3SMSL>
2026-03-26 19:24:18 +0500

    В теории должно заработать

--- Content.Server.Database/Model.cs
+++ Content.Server.Database/Model.cs
@@ -493,11 +493,7 @@ namespace Content.Server.Database
         public int Id { get; set; }
         public Profile Profile { get; set; } = null!;
         public int ProfileId { get; set; }
-
-        [MaxLength(100)]
         public string JobName { get; set; } = null!;
-
-        [MaxLength(100)]
         public string Title { get; set; } = null!;
     }
     // Sunrise-End
