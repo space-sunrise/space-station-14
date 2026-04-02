edaa319f43ae15e68750ffbb4bd41fe4846d44dd
Fooksy2304 <vip.jpek@mail.ru>
2026-04-02 18:00:34 +0600

    задача

--- Content.Shared/_Sunrise/Medical/CrewMonitoring/CrewMonitoringFilterComponent.cs
+++ Content.Shared/_Sunrise/Medical/CrewMonitoring/CrewMonitoringFilterComponent.cs
@@ -1,22 +1,56 @@
+using Robust.Shared.GameStates;
+
 namespace Content.Shared.Medical.CrewMonitoring;
 
 [RegisterComponent]
 public sealed partial class CrewMonitoringFilterComponent : Component
 {
     /// <summary>
-    /// Разрешенные отделы. Если пустое все доступны
+    /// Разрешенные отделы. Если пусто – все доступны.
     /// </summary>
-    [DataField]
+    [DataField("allowedDepartmentIds")]
     public List<string> AllowedDepartmentIds = new();
+
     /// <summary>
-    /// Будут ли отображаться по трекерам
+    /// Будут ли отображаться трекеры (импланты).
     /// </summary>
-    [DataField]
+    [DataField("includeTrackers")]
     public bool IncludeTrackers;
+
+    ///<summary>
+    /// Показывать здоровых (урон 0% – 13.2%)
     /// <summary>
-    /// Показывать ли мертвых, в крите, в ужасном
-    /// </summary>
-    [DataField]
-    public bool OnlyShowWoundedOrDead;
+    [DataField("showHealthy")]
+    public bool ShowHealthy;
+    ///<summary>
+    ///Показывать состояние "хорошо" (урон 13.2% – 36%)
+    ///<summary>
+    [DataField("showGood")]
+    public bool ShowGood;
+    ///<summary>
+    ///Показывать состояние "не очень" (урон 36% – 60%)
+    ///<summary>
+    [DataField("showNotGreat")]
+    public bool ShowNotGreat;
+    ///<summary>
+    ///Показывать состояние "плохо" (урон 60% – 83%)
+    ///<summary>
+    [DataField("showBad")]
+    public bool ShowBad;
+    ///<summary>
+    ///Показывать состояние "ужасно" (урон 83% – 100%)
+    ///<summary>
+    [DataField("showTerrible")]
+    public bool ShowTerrible;
+    ///<summary>
+    ///Показывать критическое состояние (урон >= 100%)
+    ///<summary>
+    [DataField("showCritical")]
+    public bool ShowCritical;
+    ///<summary>
+    ///Показывать мёртвых
+    ///<summary>
+    [DataField("showDead")]
+    public bool ShowDead;
 }
 
