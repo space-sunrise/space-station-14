7eeb3858d74c0019ab4c6c748ca58cb07328081d
Moony <moony@hellomouse.net>
2026-04-03 11:40:21 -0700

    Remove unused space worldgen. (#43428)
    * Remove my unmaintained feature.
    
    * Cut world loaders.
    
    * Cut unused pool.
    
    (cherry picked from commit be9b2fbb88dd679441ec15390dac295ffdeb3b24)

--- Content.Server/IoC/ServerContentIoC.cs
+++ Content.Server/IoC/ServerContentIoC.cs
@@ -29,7 +29,6 @@ using Content.Server.Preferences.Managers;
 using Content.Server.ServerInfo;
 using Content.Server.ServerUpdates;
 using Content.Server.Voting.Managers;
-using Content.Server.Worldgen.Tools;
 using Content.Shared.Administration.Logs;
 using Content.Shared.Administration.Managers;
 using Content.Shared.Chat;
@@ -71,7 +70,6 @@ internal static class ServerContentIoC
         deps.Register<PlayTimeTrackingManager>();
         deps.Register<UserDbDataManager>();
         deps.Register<ServerInfoManager>();
-        deps.Register<PoissonDiskSampler>();
         deps.Register<DiscordWebhook>();
         deps.Register<VoteWebhooks>();
         deps.Register<ServerDbEntryManager>();
