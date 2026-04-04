b2ac72bd72c1532150c8239c4a321edd215a9270
KaiserMaus <kaiser.ratte@gmail.com>
2026-04-04 23:41:39 +0100

    Fix and Update

--- Content.Server/_Starlight/EdgeConnection/EdgeConnectionSystem.cs
+++ Content.Server/_Starlight/EdgeConnection/EdgeConnectionSystem.cs
@@ -21,6 +21,7 @@ public sealed class EdgeConnectionSystem : EntitySystem
         SubscribeLocalEvent<EdgeConnectionComponent, ComponentShutdown>(OnShutdown);
         SubscribeLocalEvent<EdgeConnectionComponent, ComponentInit>(OnInit);
         SubscribeLocalEvent<EdgeConnectionComponent, EntityTerminatingEvent>(OnTerminating);
+        SubscribeLocalEvent<EdgeConnectionComponent, MoveEvent>(OnMove);
     }
 
     private void OnInit(Entity<EdgeConnectionComponent> ent, ref ComponentInit args)
@@ -47,10 +48,19 @@ public sealed class EdgeConnectionSystem : EntitySystem
         UpdateNeighbors(ent);
     }
 
+    private void OnMove(Entity<EdgeConnectionComponent> ent, ref MoveEvent args)
+    {
+        if (args.NewRotation == args.OldRotation)
+            return;
+
+        UpdateConnections(ent);
+        UpdateNeighbors(ent);
+    }
+
     private void UpdateConnections(Entity<EdgeConnectionComponent> ent)
     {
         var xform = Transform(ent);
-        
+
         if (!xform.Anchored || !TryComp<MapGridComponent>(xform.GridUid, out var grid))
         {
             _appearance.SetData(ent, EdgeConnectionVisuals.ConnectionMask, EdgeConnectionFlags.None);
@@ -61,38 +71,70 @@ public sealed class EdgeConnectionSystem : EntitySystem
         var tile = _map.TileIndicesFor(xform.GridUid.Value, grid, xform.Coordinates);
         var allowed = ent.Comp.AllowedDirections;
 
-        // Check each allowed direction
-        if ((allowed & EdgeConnectionFlags.East) != 0)
-        {
-            if (HasMatchingNeighbor(ent, xform.GridUid.Value, grid, tile + new Vector2i(1, 0), ent.Comp.ConnectionKey))
-                mask |= EdgeConnectionFlags.East;
-        }
-
-        if ((allowed & EdgeConnectionFlags.West) != 0)
-        {
-            if (HasMatchingNeighbor(ent, xform.GridUid.Value, grid, tile + new Vector2i(-1, 0), ent.Comp.ConnectionKey))
-                mask |= EdgeConnectionFlags.West;
-        }
-
-        if ((allowed & EdgeConnectionFlags.North) != 0)
-        {
-            if (HasMatchingNeighbor(ent, xform.GridUid.Value, grid, tile + new Vector2i(0, 1), ent.Comp.ConnectionKey))
-                mask |= EdgeConnectionFlags.North;
-        }
-
-        if ((allowed & EdgeConnectionFlags.South) != 0)
-        {
-            if (HasMatchingNeighbor(ent, xform.GridUid.Value, grid, tile + new Vector2i(0, -1), ent.Comp.ConnectionKey))
-                mask |= EdgeConnectionFlags.South;
-        }
+        TrySetConnectionLocal(ent, ref mask, allowed, EdgeConnectionFlags.East, tile, xform.GridUid.Value, grid, xform.LocalRotation);
+        TrySetConnectionLocal(ent, ref mask, allowed, EdgeConnectionFlags.West, tile, xform.GridUid.Value, grid, xform.LocalRotation);
+        TrySetConnectionLocal(ent, ref mask, allowed, EdgeConnectionFlags.North, tile, xform.GridUid.Value, grid, xform.LocalRotation);
+        TrySetConnectionLocal(ent, ref mask, allowed, EdgeConnectionFlags.South, tile, xform.GridUid.Value, grid, xform.LocalRotation);
 
         _appearance.SetData(ent, EdgeConnectionVisuals.ConnectionMask, mask);
+    }
+
+    private void TrySetConnectionLocal(
+        Entity<EdgeConnectionComponent> ent,
+        ref EdgeConnectionFlags mask,
+        EdgeConnectionFlags allowedLocal,
+        EdgeConnectionFlags localDirection,
+        Vector2i tile,
+        EntityUid gridUid,
+        MapGridComponent grid,
+        Angle localRotation)
+    {
+        if ((allowedLocal & localDirection) == 0)
+            return;
+
+        var worldDirection = LocalToWorldDirection(localDirection, localRotation);
+        var offset = DirectionToOffset(worldDirection);
+        var neighborTile = tile + offset;
+
+        if (!HasMatchingNeighbor(ent, gridUid, grid, neighborTile, ent.Comp.ConnectionKey))
+            return;
+
+        mask |= localDirection;
+    }
+
+    private static Direction LocalToWorldDirection(EdgeConnectionFlags localDirection, Angle localRotation)
+    {
+        var localDir = localDirection switch
+        {
+            EdgeConnectionFlags.East => Direction.East,
+            EdgeConnectionFlags.West => Direction.West,
+            EdgeConnectionFlags.North => Direction.North,
+            EdgeConnectionFlags.South => Direction.South,
+            _ => Direction.Invalid,
+        };
+
+        if (localDir == Direction.Invalid)
+            return Direction.Invalid;
+
+        return localRotation.RotateDir(localDir);
+    }
+
+    private static Vector2i DirectionToOffset(Direction direction)
+    {
+        return direction switch
+        {
+            Direction.East => new Vector2i(1, 0),
+            Direction.West => new Vector2i(-1, 0),
+            Direction.North => new Vector2i(0, 1),
+            Direction.South => new Vector2i(0, -1),
+            _ => Vector2i.Zero
+        };
     }
 
     private bool HasMatchingNeighbor(EntityUid entity, EntityUid gridUid, MapGridComponent grid, Vector2i tile, string key)
     {
         var anchored = _map.GetAnchoredEntitiesEnumerator(gridUid, grid, tile);
-        
+
         while (anchored.MoveNext(out var other))
         {
             if (other == entity)
@@ -111,7 +153,7 @@ public sealed class EdgeConnectionSystem : EntitySystem
     private void UpdateNeighbors(Entity<EdgeConnectionComponent> ent)
     {
         var xform = Transform(ent);
-        
+
         if (!TryComp<MapGridComponent>(xform.GridUid, out var grid))
             return;
 
@@ -127,7 +169,7 @@ public sealed class EdgeConnectionSystem : EntitySystem
     private void UpdateNeighborsAtTile(EntityUid gridUid, MapGridComponent grid, Vector2i tile)
     {
         var anchored = _map.GetAnchoredEntitiesEnumerator(gridUid, grid, tile);
-        
+
         while (anchored.MoveNext(out var other))
         {
             if (TryComp<EdgeConnectionComponent>(other, out var comp))
--- Resources/Locale/en-US/_prototypes/_starlight/entities/objects/weapons/guns/ammunition/explosives.ftl
+++ Resources/Locale/en-US/_prototypes/_starlight/entities/objects/weapons/guns/ammunition/explosives.ftl
@@ -4,6 +4,9 @@
 ent-Grenade120mmBlast = 120mm blast grenade
     .desc = A devastating 120mm blast grenade designed for the CYERXA cannon system. Three times more powerful than standard blast grenades.
 
+ent-Rocket120mmMetlaLongFlight = 120mm long-flight rocket
+    .desc = A heavy 120mm rocket for the RCT-120 Metla launcher with extended powered flight time.
+
 ent-Magazine120mmGrenadeBlast = 120mm blast grenade magazine
     .desc = A large 20-round magazine loaded with powerful blast grenades for the CYERXA cannon system.
 
