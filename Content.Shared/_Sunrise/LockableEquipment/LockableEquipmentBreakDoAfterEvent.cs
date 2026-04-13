01cfbe4a387cb2912a796e3de52676ea4586cb76
MataVsn <113342160+MataVsn@users.noreply.github.com>
2026-04-13 21:09:35 +0300

    Update Content.Shared/_Sunrise/LockableEquipment/LockableEquipmentBreakDoAfterEvent.cs
    Co-authored-by: iertis <kanopus952@gmail.com>

--- Content.Shared/_Sunrise/LockableEquipment/LockableEquipmentBreakDoAfterEvent.cs
+++ Content.Shared/_Sunrise/LockableEquipment/LockableEquipmentBreakDoAfterEvent.cs
@@ -4,6 +4,4 @@ using Robust.Shared.Serialization;
 namespace Content.Shared._Sunrise.LockableEquipment;
 
 [Serializable, NetSerializable]
-public sealed partial class LockableEquipmentBreakDoAfterEvent : SimpleDoAfterEvent
-{
-}
+public sealed partial class LockableEquipmentBreakDoAfterEvent : SimpleDoAfterEvent;
