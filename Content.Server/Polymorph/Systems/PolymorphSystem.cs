8dcab2b19c1b640da5e42038fa31b3c409b725da
Orvex07 <77834935+Orvex07@users.noreply.github.com>
2026-06-06 19:01:35 +0200

    Update PolymorphSystem.cs

--- Content.Server/Polymorph/Systems/PolymorphSystem.cs
+++ Content.Server/Polymorph/Systems/PolymorphSystem.cs
@@ -2,6 +2,7 @@ using Content.Server.Actions;
 using Content.Server.Humanoid;
 using Content.Server.Inventory;
 using Content.Server.Polymorph.Components;
+using Content.Server._Sunrise.Movement.Carrying;
 using Content.Shared.Buckle;
 using Content.Shared.Coordinates;
 using Content.Shared.Damage.Components;
@@ -12,20 +13,18 @@ using Content.Shared.IdentityManagement;
 using Content.Shared.Mind;
 using Content.Shared.Mobs.Components;
 using Content.Shared.Mobs.Systems;
+using Content.Shared.Mind.Components;
 using Content.Shared.Nutrition;
 using Content.Shared.Polymorph;
 using Content.Shared.Popups;
+using Content.Shared._Sunrise.Movement.Carrying;
 using Robust.Server.Audio;
 using Robust.Server.Containers;
 using Robust.Server.GameObjects;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Timing;
 using Robust.Shared.Utility;
-// Sunrise-Start
-using Content.Server._Sunrise.Movement.Carrying;
-using Content.Shared.Mind.Components;
-using Content.Shared._Sunrise.Movement.Carrying;
-// Sunrise-End
+
 
 namespace Content.Server.Polymorph.Systems;
 
