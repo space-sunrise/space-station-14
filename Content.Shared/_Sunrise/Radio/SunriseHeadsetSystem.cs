e8f657a4fb0c3f95e756c3849ff90df49ba52a1c
KaiserMaus <kaiser.ratte@gmail.com>
2026-06-08 10:30:29 +0100

    revert CommonChannel false

--- Content.Shared/_Sunrise/Radio/SunriseHeadsetSystem.cs
+++ Content.Shared/_Sunrise/Radio/SunriseHeadsetSystem.cs
@@ -26,7 +26,7 @@ public sealed class SunriseHeadsetSystem : EntitySystem
     {
         if (TryComp<HeadsetComponent>(ent, out var headset) &&
             args.Component.Channels.Contains(SharedChatSystem.CommonChannel) &&
-            headset.EnabledChannels.TryAdd(SharedChatSystem.CommonChannel.Id, true))
+            headset.EnabledChannels.TryAdd(SharedChatSystem.CommonChannel.Id, false))
         {
             Dirty(ent, headset);
         }
