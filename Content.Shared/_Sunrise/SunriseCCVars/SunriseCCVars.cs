6e1f4a21606f396c6d077f9b79eb08f36add09c5
drdth <drdtheuser@gmail.com>
2026-06-14 22:50:55 +0300

    Revert "Отключение анимации лежание до момента настоящего фикса (#4514)"
    This reverts commit 586341cd17ff7b9f0131570c8314b924bf14c5a9.

--- Content.Shared/_Sunrise/SunriseCCVars/SunriseCCVars.cs
+++ Content.Shared/_Sunrise/SunriseCCVars/SunriseCCVars.cs
@@ -576,6 +576,5 @@ public sealed partial class SunriseCCVars : CVars
     public static readonly CVarDef<TimeSpan> TutorialCooldown =
         CVarDef.Create("tutorial.cooldown", TimeSpan.FromSeconds(15), CVar.SERVERONLY);
 
-    public static readonly CVarDef<bool> LyingAnimationEnabled =
-        CVarDef.Create("lying.animation_enabled", false, CVar.REPLICATED | CVar.ARCHIVE);
+
 }
