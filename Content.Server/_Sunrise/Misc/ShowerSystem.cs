cad8ef7754bc28a14e4abd694a643d0454634e3c
liegend <2008.morozov.ri@gmail.com>
2026-06-15 22:06:18 +0700

    мееем

--- Content.Server/_Sunrise/Misc/ShowerSystem.cs
+++ Content.Server/_Sunrise/Misc/ShowerSystem.cs
@@ -1,4 +1,7 @@
+using Content.Server.Fluids.EntitySystems;
 using Content._Sunrise.Shared.Shower;
+using Content.Shared.Chemistry.Components;
+using Content.Shared.FixedPoint;
 using Content.Shared.Interaction;
 
 namespace Content._Sunrise.Server.Shower;
@@ -6,11 +9,14 @@ namespace Content._Sunrise.Server.Shower;
 public sealed class ShowerSystem : EntitySystem
 {
     [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
+    [Dependency] private readonly PuddleSystem _puddle = default!;
 
     private float _accumulator = 0f;
-    private const float UpdateInterval = 10f;
-    private const string PuddlePrototype = "PuddleWater";
-    private EntityUid? _currentPuddle;
+    private const float UpdateInterval = 5f;
+    private const float ShowerFillDuration = 120f;
+    private const float ShowerFillLimit = 200f;
+    private const string ShowerReagent = "Water";
+    private static readonly FixedPoint2 ShowerSpillAmount = FixedPoint2.New(ShowerFillLimit / (ShowerFillDuration / UpdateInterval));
 
     public override void Initialize()
     {
@@ -26,10 +32,7 @@ public sealed class ShowerSystem : EntitySystem
 
         ent.Comp.IsActive = !ent.Comp.IsActive;
         _appearance.SetData(ent.Owner, ShowerVisuals.Active, ent.Comp.IsActive);
-        if (_currentPuddle == null || Deleted(_currentPuddle))
-        {
-            _currentPuddle = Spawn(PuddlePrototype, Transform(ent).Coordinates);
-        }
+
         Dirty(ent);
     }
 
@@ -49,7 +52,12 @@ public sealed class ShowerSystem : EntitySystem
             if (!shower.IsActive)
                 continue;
 
-            Spawn(PuddlePrototype, Transform(uid).Coordinates);
+            SpillWater((uid, shower));
         }
+    }
+
+    private void SpillWater(Entity<ShowerComponent> ent)
+    {
+        _puddle.TrySpillAt(ent, new Solution(ShowerReagent, ShowerSpillAmount), out _);
     }
 }
