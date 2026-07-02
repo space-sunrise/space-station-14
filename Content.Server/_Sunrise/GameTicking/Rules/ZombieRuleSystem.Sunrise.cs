7b8991e15fd7ead9f4a56325ec80e95f75468d08
KaiserMaus <kaiser.ratte@gmail.com>
2026-07-02 12:17:17 +0100

    rabbiut

--- Content.Server/_Sunrise/GameTicking/Rules/ZombieRuleSystem.Sunrise.cs
+++ Content.Server/_Sunrise/GameTicking/Rules/ZombieRuleSystem.Sunrise.cs
@@ -5,23 +5,54 @@ namespace Content.Server.GameTicking.Rules;
 
 public sealed partial class ZombieRuleSystem
 {
-    private static readonly TimeSpan ZombieIcarusBeamDelay = TimeSpan.FromSeconds(25);
-
     [Dependency] private readonly GameTicker _gameTicker = default!;
     [Dependency] private readonly IcarusTerminalSystem _icarus = default!;
 
+    private static readonly TimeSpan ZombieIcarusBeamDelay = TimeSpan.FromSeconds(25);
+
     private void CheckSunriseInfectedThresholds(ZombieRuleComponent zombieRuleComponent, float infectedFraction)
     {
-        if (infectedFraction >= zombieRuleComponent.ZombieCburnCallPercentage && !zombieRuleComponent.CburnCalled)
-        {
-            _gameTicker.StartGameRule(zombieRuleComponent.ZombieCburnGameRule);
-            zombieRuleComponent.CburnCalled = true;
-        }
+        TryCallCburn(zombieRuleComponent, infectedFraction);
+        TryCallIcarusBeam(zombieRuleComponent, infectedFraction);
+    }
 
-        if (infectedFraction >= zombieRuleComponent.ZombieIcarusBeamPercentage && !zombieRuleComponent.IcarusBeamCalled)
-        {
-            _icarus.FireBeamOnStationDelayed(ZombieIcarusBeamDelay);
-            zombieRuleComponent.IcarusBeamCalled = true;
-        }
+    private bool TryCallCburn(ZombieRuleComponent zombieRuleComponent, float infectedFraction)
+    {
+        if (!CanCallCburn(zombieRuleComponent, infectedFraction))
+            return false;
+
+        DoCallCburn(zombieRuleComponent);
+        return true;
+    }
+
+    private bool CanCallCburn(ZombieRuleComponent zombieRuleComponent, float infectedFraction)
+    {
+        return infectedFraction >= zombieRuleComponent.ZombieCburnCallPercentage && !zombieRuleComponent.CburnCalled;
+    }
+
+    private void DoCallCburn(ZombieRuleComponent zombieRuleComponent)
+    {
+        _gameTicker.StartGameRule(zombieRuleComponent.ZombieCburnGameRule);
+        zombieRuleComponent.CburnCalled = true;
+    }
+
+    private bool TryCallIcarusBeam(ZombieRuleComponent zombieRuleComponent, float infectedFraction)
+    {
+        if (!CanCallIcarusBeam(zombieRuleComponent, infectedFraction))
+            return false;
+
+        DoCallIcarusBeam(zombieRuleComponent);
+        return true;
+    }
+
+    private bool CanCallIcarusBeam(ZombieRuleComponent zombieRuleComponent, float infectedFraction)
+    {
+        return infectedFraction >= zombieRuleComponent.ZombieIcarusBeamPercentage && !zombieRuleComponent.IcarusBeamCalled;
+    }
+
+    private void DoCallIcarusBeam(ZombieRuleComponent zombieRuleComponent)
+    {
+        _icarus.FireBeamOnStationDelayed(ZombieIcarusBeamDelay);
+        zombieRuleComponent.IcarusBeamCalled = true;
     }
 }
