8b01b9ce651bfb796a8628116f17ddc4ec3a2b92
drdth <drdtheuser@gmail.com>
2026-07-11 04:23:33 +0300

    fix: delete vibecoded tests, the first step to the great goal

--- Content.IntegrationTests/Pair/TestPair.cs
+++ Content.IntegrationTests/Pair/TestPair.cs
@@ -1,9 +1,7 @@
 ﻿#nullable enable
 using System.Collections.Generic;
-using Robust.Client.Audio.Midi;
 using Content.Client.IoC;
 using Content.Client.Parallax.Managers;
-using Content.IntegrationTests._Sunrise;
 using Content.IntegrationTests.Tests.Destructible;
 using Content.IntegrationTests.Tests.DeviceNetwork;
 using Content.Server.GameTicking;
@@ -94,15 +92,6 @@ public sealed partial class TestPair : RobustIntegrationTest.TestPair
         {
             LoadConfigAndUserData = false,
         };
-
-        // Sunrise edit start - подменяем клиентский MIDI backend до сборки IoC graph, чтобы instrument tests оставались headless
-        var previousInitIoC = opts.InitIoC;
-        opts.InitIoC = () =>
-        {
-            previousInitIoC?.Invoke();
-            IoCManager.Register<IMidiManager, DummyMidiManager>(true);
-        };
-        // Sunrise edit end
 
         opts.BeforeStart += () =>
         {
