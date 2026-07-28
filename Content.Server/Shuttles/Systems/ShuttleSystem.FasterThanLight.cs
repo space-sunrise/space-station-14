30197185e6350e8d17c4c083e3c59a6c1c14dd7d
Ligyb <ligybs@gmail.com>
2026-07-28 17:17:47 +0500

    Update ShuttleSystem.FasterThanLight.cs

--- Content.Server/Shuttles/Systems/ShuttleSystem.FasterThanLight.cs
+++ Content.Server/Shuttles/Systems/ShuttleSystem.FasterThanLight.cs
@@ -584,15 +584,12 @@ public sealed partial class ShuttleSystem
         else if (HasComp<MapGridComponent>(target.EntityId) &&
                  !HasComp<MapComponent>(target.EntityId))
         {
-            // Sunrise-Start
-            DockingConfig? config;
-            if (HasComp<SunriseArrivalsShuttleComponent>(uid))
-                config = _dockSystem.GetDockingConfigAt(uid, target.EntityId, target, entity.Comp1.TargetAngle,
-                    ignored: false, priorityTag: entity.Comp1.PriorityTag);
-            else
-                config = _dockSystem.GetDockingConfigAt(uid, target.EntityId, target, entity.Comp1.TargetAngle,
-                    entity.Comp1.Ignored, priorityTag: entity.Comp1.PriorityTag);
-            // Sunrise-End
+            // Sunrise edit start - Ignored был позиционным аргументом, кто-то его перепутал и он устанавливал fallback, а ignored по дефолту оставался true
+            // из-за этого шаттлы всегда накладывались друг на друга, даже если это не нужно
+            var ignored = !HasComp<SunriseArrivalsShuttleComponent>(uid) && entity.Comp1.Ignored;
+            var config = _dockSystem.GetDockingConfigAt(uid, target.EntityId, target, entity.Comp1.TargetAngle,
+                ignored: ignored, priorityTag: entity.Comp1.PriorityTag);
+            // Sunrise edit end
             var mapCoordinates = _transform.ToMapCoordinates(target);
 
             // Couldn't dock somehow so just fallback to regular position FTL.
