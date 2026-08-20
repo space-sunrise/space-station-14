0c5f6fb8441a1b6c831b9d309524f6990b7b7d96
iertis <kanopus952@gmail.com>
2026-08-20 19:05:21 +0300

    ыы фикс

--- Content.Server/Chat/Systems/ChatSystem.cs
+++ Content.Server/Chat/Systems/ChatSystem.cs
@@ -63,7 +63,7 @@ public sealed partial class ChatSystem : SharedChatSystem
     [Dependency] private readonly ActionBlockerSystem _actionBlocker = default!;
     [Dependency] private readonly StationSystem _stationSystem = default!;
     [Dependency] private readonly MobStateSystem _mobStateSystem = default!;
-    [Dependency] private readonly SharedAudioSystem _audio = default!;
+    // [Dependency] private readonly SharedAudioSystem _audio = default!; // Мы не используем этот депенси
     [Dependency] private readonly ReplacementAccentSystem _wordreplacement = default!;
     [Dependency] private readonly ExamineSystemShared _examineSystem = default!;
     [Dependency] private readonly AnnouncementSpeakerSystem _announcementSpeaker = default!;
